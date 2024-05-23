4747e02ad01d4cf542e9b89559fa9b9d340b710a
Stefan Gheorghe <sgheorghe@T5PT483.AUSYNET.FR>
2024-05-23 21:19:57 +0300

    adding UT for GenerateFrame

--- src/mcu/test/GenerateFrame_Test.cpp
+++ src/mcu/test/GenerateFrame_Test.cpp
@@ -1,6 +1,7 @@
 #include "../include/GenerateFrame.h"
 #include <gtest/gtest.h>
 #include <fcntl.h>
+#include <stdexcept>
 
 class CANFrameTest : public testing::Test {
 protected:
@@ -54,34 +55,90 @@ TEST_F(CANFrameTest, ConstructDataFrame) {
     }
 }
 
-// TEST_F(CANFrameTest, SendDataFrame) {
-//     const uint32_t can_id = 0x123;
-//     const uint8_t data[] = {0x01, 0x02, 0x03};
-//     const uint8_t dlc = sizeof(data) / sizeof(data[0]);
+TEST_F(CANFrameTest, ConstructRemoteFrame) {
+    const uint32_t can_id = 0x123;
+    const uint8_t dlc = 3; 
 
-//     CANFrame frame(FrameType::DATA_FRAME, can_id, data, dlc);
+    CANFrame frame(FrameType::REMOTE_FRAME, can_id, nullptr, dlc);
 
-//     EXPECT_EQ(frame.SendFrame("vcan0", s), 0);
-//     // Need to wait for the frame to be sent before checking
-//     usleep(1000); // Wait for 1ms
+    EXPECT_EQ(frame.frame.can_id, can_id | CAN_RTR_FLAG);
+    EXPECT_EQ(frame.frame.can_dlc, dlc);
+}
 
-//     struct can_frame received_frame;
-//     int nbytes = read(s, &received_frame, sizeof(struct can_frame));
+TEST_F(CANFrameTest, ConstructErrorFrame) {
+    const uint32_t can_id = 0x123;
 
-//     EXPECT_EQ(nbytes, sizeof(struct can_frame));
-//     EXPECT_EQ(received_frame.can_id, can_id);
-//     EXPECT_EQ(received_frame.can_dlc, dlc);
-//     for (int i = 0; i < dlc; ++i) {
-//         EXPECT_EQ(received_frame.data[i], data[i]);
-//     }
-// }
+    CANFrame frame(FrameType::ERROR_FRAME, can_id, nullptr, 0);
 
-TEST_F(CANFrameTest, SendFrameSuccess) {
+    EXPECT_EQ(frame.frame.can_id, CAN_ERR_FLAG);
+    EXPECT_EQ(frame.frame.can_dlc, 0);
+}
+
+TEST_F(CANFrameTest, ConstructOverloadFrame) {
+    const uint32_t can_id = 0x123;
+
+    CANFrame frame(FrameType::OVERLOAD_FRAME, can_id, nullptr, 0);
+
+    EXPECT_EQ(frame.frame.can_id, CAN_ERR_FLAG);
+    EXPECT_EQ(frame.frame.can_dlc, 0);
+}
+
+TEST_F(CANFrameTest, InvalidFrameType) {
+    const uint32_t can_id = 0x123;
+    const uint8_t data[] = {0x01, 0x02, 0x03};
+    const uint8_t dlc = sizeof(data) / sizeof(data[0]);
+
+    EXPECT_THROW(CANFrame frame(static_cast<FrameType>(-1), can_id, data, dlc), std::invalid_argument);
+}
+
+TEST_F(CANFrameTest, GetFrame) {
     const uint32_t can_id = 0x123;
     const uint8_t data[] = {0x01, 0x02, 0x03};
     const uint8_t dlc = sizeof(data) / sizeof(data[0]);
 
     CANFrame frame(FrameType::DATA_FRAME, can_id, data, dlc);
+    struct can_frame retrieved_frame = frame.getFrame();
+
+    EXPECT_EQ(retrieved_frame.can_id, can_id);
+    EXPECT_EQ(retrieved_frame.can_dlc, dlc);
+    for (int i = 0; i < dlc; ++i) {
+        EXPECT_EQ(retrieved_frame.data[i], data[i]);
+    }
+}
+
+TEST_F(CANFrameTest, SendDataFrame) {
+    const uint32_t can_id = 0x123;
+    const uint8_t data[] = {0x01, 0x02, 0x03};
+    const uint8_t dlc = sizeof(data) / sizeof(data[0]);
+
+    CANFrame frame(FrameType::DATA_FRAME, can_id, data, dlc);
+
+    EXPECT_EQ(frame.SendFrame("vcan0", s), 0);
+}
+
+TEST_F(CANFrameTest, SendRemoteFrame) {
+    const uint32_t can_id = 0x123;
+    const uint8_t dlc = 0; // Remote frame has no data
+
+    CANFrame frame(FrameType::REMOTE_FRAME, can_id, nullptr, dlc);
+
+    EXPECT_EQ(frame.SendFrame("vcan0", s), 0);
+}
+
+TEST_F(CANFrameTest, SendErrorFrame) {
+    const uint32_t can_id = 0x123;
+    const uint8_t dlc = 0; // Error frame has no data
+
+    CANFrame frame(FrameType::ERROR_FRAME, can_id, nullptr, dlc);
+
+    EXPECT_EQ(frame.SendFrame("vcan0", s), 0);
+}
+
+TEST_F(CANFrameTest, SendOverloadFrame) {
+    const uint32_t can_id = 0x123;
+    const uint8_t dlc = 0; // Overload frame has no data
+
+    CANFrame frame(FrameType::OVERLOAD_FRAME, can_id, nullptr, dlc);
 
     EXPECT_EQ(frame.SendFrame("vcan0", s), 0);
 }
@@ -96,4 +153,9 @@ TEST_F(CANFrameTest, SendFrameFailure) {
     // Invalid socket descriptor
     int invalid_socket = -1;
     EXPECT_EQ(frame.SendFrame("vcan0", invalid_socket), -1);
+}
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
