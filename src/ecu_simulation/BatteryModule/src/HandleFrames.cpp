c8074e57622f858b9fac6f13e0ee747d4564197d
Miruna-Maria <miruna.stoisor@randstaddigital.com>
2024-05-23 13:49:25 +0300

    updated ReceiveFrames and HandleFrames

--- src/ecu_simulation/BatteryModule/src/HandleFrames.cpp
+++ src/ecu_simulation/BatteryModule/src/HandleFrames.cpp
@@ -62,12 +62,46 @@ void HandleFrames::ProcessReceivedFrame(const struct can_frame &frame) {
             }  
             break;
         case 0x22: // ReadDataByIdentifier method --to be implemented
-            std::cout << "Service 0x22 ReadDataByIdentifier\n";
+            std::cout << "Service 0x22 ReadDataByIdentifierLongResponseFF\n";
             if (isFirstFrame) {
-                //ReadDataByIdentifierLongResponse(id, data, sid_position);
+                // Verify that there are enough elements in the data vector
+                if (data.size() >= 8) { // Ensure there are at least 5 elements in the data vector
+                    // Extract identifier from the data vector
+                    int identifier = (data[3] << 8) | data[4];
+
+                    // Extract the remaining data after the identifier
+                    std::vector<int> rdata(data.begin() + 5, data.begin() + 8);
+                    std::cout << "Valid data length\n";
+                    // Store the first part of the data to be concatenated later
+                    stored_data = rdata;
+                    // Store the expected data size
+                    expected_data_size = data[1];
+                    //ReadDataByIdentifierLongResponse(id, identifier, rdata, isFirstFrame);
+                } else {
+                    // Print an error message if there are not enough elements in the data vector
+                    std::cerr << "Invalid data length for ReadDataByIdentifierLongResponse\n";
+                }
             } else if (isConsecutiveFrame) {
-                //HandleConsecutiveFrame(id, pci, data);
+                std::cout << "Service 0x22 ReadDataByIdentifierLongResponseCF\n";
+                // Verify that there are enough elements in the data vector
+                if (data.size() > 1) { // Ensure there are at least 5 elements in the data vector
+                    std::vector<int> rdata(data.begin() + 1, data.end()); // Skip the first byte which is the frame indicator
+                    stored_data.insert(stored_data.end(), rdata.begin(), rdata.end());
+
+                    // Once all parts are received, call the appropriate function
+                    if (stored_data.size() == expected_data_size) {
+                        int identifier = (stored_data[0] << 8) | stored_data[1];
+                        std::vector<int> final_data(stored_data.begin() + 2, stored_data.end());
+                        // Print a message indicating valid data length
+                        std::cout << "Valid data length\n";
+                        //ReadDataByIdentifierLongResponse(id, identifier, final_data, false);
+                    }
+                } else {
+                    // Print an error message if there are not enough elements in the data vector
+                    std::cerr << "Invalid data length for ReadDataByIdentifierLongResponse\n";
+                }
             } else {
+                std::cout << "Service 0x22 ReadDataByIdentifier\n";
                 if (sid_position + 3 < data.size()) {  //sub_function data always found next to sid, has to be in valid data range
                 int identifier = (data[sid_position + 1] << 8) | data[sid_position + 2];
                 std::vector<int> rdata(data.begin() + sid_position + 3, data.end());
@@ -79,9 +113,7 @@ void HandleFrames::ProcessReceivedFrame(const struct can_frame &frame) {
                 } 
             }
             break;
-        case 0x03:// ReadMemoryByAdressLongResponse method --to be implemented
-            std::cout << "Service 0x29 ReadMemoryByAdressLongResponse\n";
-            break;
+        
         case 0x27: // AuthenticationRequestSeed method --to be implemented
             if (data.size() > 2) {
                 std::cout << "Service 0x27-n AuthenticationRequestSeed\n";
@@ -94,36 +126,84 @@ void HandleFrames::ProcessReceivedFrame(const struct can_frame &frame) {
         case 0x29: // AuthenticationSendKey method --to be implemented
             std::cout << "Service 0x29 AuthenticationSendKey\n";
             //AuthenticationSendKey(frame.can_id, std::vector<int>(data.begin() + 2, data.end()));
+            std::cout << "Service 0x29 AuthenticationSendKey\n";
+            if (sid_position + 1 < data.size()) {  
+                int sub_function = data[sid_position + 1];
+                std::cout << "Valid data length\n";
+                std::vector<int> key(data.begin() + sid_position + 2, data.end()); // Extract key from data
+                //AuthenticationSendKey(id, key, false); // Call the method to send authentication key
+        } else {
+            std::cerr << "Invalid data length for AuthenticationSendKey\n";
+        } 
+        break;
             break;
         case 0x31: // RoutineControl method --to be implemented
             std::cout << "Service 0x31 RoutineControl\n";
-            //RoutineControl(id);
+            if (sid_position + 1 < data.size()) {  // sub_function data always found next to sid, has to be in valid data range
+                int sub_function = data[sid_position + 1];
+                std::cout << "Valid data length\n";
+                // RoutineControl(id, sub_function, routin_identifier, response);
+            } else {
+                std::cerr << "Invalid data length for RoutineControl\n";
+    }
             break;
         case 0x3E: // TesterPresent method --to be implemented
             std::cout << "Service 0x3E TesterPresent\n";
-            //TesterPresent(id);
+                if (sid_position + 1 < data.size()) {  // sub_function data always found next to sid, has to be in valid data range
+                    int sub_function = data[sid_position + 1];
+                    std::cout << "Valid data length\n";
+                    // Call the appropriate function based on the sub-function
+                    // TesterPresent(id, sub_function);
+                } else {
+                    std::cerr << "Invalid data length for TesterPresent\n";
+                }
             break;
         case 0x23: // ReadMemoryByAddress method --to be implemented
             std::cout << "Service 0x23 ReadMemoryByAddress\n";
             //ReadMemoryByAdress(id, (data[2] << 8) | data[3], data[4], (data[5] << 24) | (data[6] << 16) | (data[7] << 8) | data[8]);
+            // ReadMemoryByAdressLongResponse method --to be implemented
+            std::cout << "Service 0x23 ReadMemoryByAdressLongResponse\n";
             break;
         case 0x2E: // WriteDataByIdentifier method --to be implemented
             std::cout << "Service 0x2E WriteDataByIdentifier\n";
             //WriteDataByIdentifier(id, (data[2] << 8) | data[3], std::vector<int>(data.begin() + 4, data.end()));
-            break;
-        case 0x04:// WriteDataByIdentifierLongData method --to be implemented
+            // WriteDataByIdentifierLongData method --to be implemented
             std::cout << "Service 0x19 WriteDataByIdentifierLongData\n";
             break;
         case 0x19: // ReadDtcInformation method --to be implemented
             std::cout << "Service 0x19 ReadDtcInformation\n";
-            //ReadDtcInformation(id, data[0] == 0x59);
+            if (sid_position + 2 < data.size()) {  // sub_function and dtc_status_mask data always found next to sid, has to be in valid data range
+                int sub_function = data[sid_position + 1];
+                int dtc_status_mask = data[sid_position + 2];
+                std::cout << "Valid data length\n";
+                // ReadDtcInformation(id, sub_function, dtc_status_mask);
+            } else {
+                std::cerr << "Invalid data length for ReadDtcInformation\n";
+            }
             break;
         case 0x05:// ReadDtcInformationResponse01 method --to be implemented
             std::cout << "Service 0x29 ReadDtcInformationResponse01\n";
             break;
-        case 0x14: // ClearDiagnosticInfirmation method --to be implemented
-            std::cout << "Service 0x14 ClearDiagnosticInfirmation\n";
-            //ClearDiagnosticInfirmation(id, data[0] == 0x54);
+        case 0x14: // ClearDiagnosticInformation method --to be implemented
+            std::cout << "Service 0x14 ClearDiagnosticInfirmationFF\n";
+            if (isFirstFrame) {
+                // Verify that there are enough elements in the data vector
+                if (data.size() >= 2) { // Ensure there are at least 2 elements in the data vector
+                    // Extract group_of_dtc from the data vector
+                    std::vector<int> group_of_dtc(data.begin() + 2, data.end());
+
+                    std::cout << "Valid data length\n";
+                    //ClearDiagnosticInformation(id, group_of_dtc, response);
+                } else {
+                    std::cerr << "Invalid data length for ClearDiagnosticInformation\n";
+                }
+            } else if (isConsecutiveFrame) {
+                std::cout << "Service 0x14 ClearDiagnosticInformationCF\n";
+                // ClearDiagnosticInformation(id, pci, data);
+            } else {
+                std::cout << "Service 0x14 ClearDiagnosticInformation\n";
+                // Process the response frame if needed
+            }
             break;
         case 0x83: // AccessTimingParameters method --to be implemented
             std::cout << "Service 0x83 AccessTimingParameters\n";
@@ -139,14 +219,27 @@ void HandleFrames::ProcessReceivedFrame(const struct can_frame &frame) {
             //RequestDownload(id, data[2], data[3], (data[4] << 24) | (data[5] << 16) | (data[6] << 8) | data[7], (data[8] << 24) | (data[9] << 16) | (data[10] << 8) | data[11]);
             break;
         case 0x36: // TransferData method --to be implemented
-            std::cout << "Service 0x36 TransferData\n";
-            //TransferData(id, (data[2] << 8) | data[3], data[4]);
-            break;
-        case 0x06:// TransferDataLong method --to be implemented
+            // TransferDataLong method --to be implemented
             std::cout << "Service 0x29 TransferDataLong\n";
+            std::cout << "Service 0x36 TransferData\n";
+            if (sid_position + 2 < data.size()) {  
+                int block_sequence_counter = data[sid_position + 1];
+                std::cout << "Valid data length\n";
+                std::vector<int> transfer_request(data.begin() + sid_position + 2, data.end()); // Extract transfer request from frame
+                //TransferData(can_id, block_sequence_counter, transfer_request); // Call the method to transfer data
+            } else {
+                std::cerr << "Invalid data length for TransferData\n";
+            }            
             break;
         case 0x37: // RequestTransferExit method --to be implemented
             std::cout << "Service 0x37 RequestTransferExit\n";
+            if (sid_position + 1 < data.size()) {  
+                std::cout << "Valid data length\n";
+                bool response = data[sid_position + 1];
+                //RequestTransferExit(id, response);
+            } else {
+                std::cerr << "Invalid data length for RequestTransferExit\n";
+            }
             //RequestTransferExit(id, data[2]);
             break;
         case 0x32: // RequestUpdateStatus method --to be implemented
--- src/ecu_simulation/BatteryModule/src/ReceiveFrames.cpp
+++ src/ecu_simulation/BatteryModule/src/ReceiveFrames.cpp
@@ -2,69 +2,115 @@
 
 #include <sstream>
 #include <iomanip>
+#include <cstring>
 #include "../include/ReceiveFrames.h"
 #include "../include/HandleFrames.h"
 
-ReceiveFrames::ReceiveFrames(int socket, int moduleID) {
-    // Print the moduleID for debugging
-    std::cout << "Module ID: " << this->moduleID << std::endl;
-    if (socket >= 0)
-    {
-        this->socket = socket;
-    } else{
-        std::cout<<"Error: Pass a valid Socket\n";
+ReceiveFrames::ReceiveFrames(int socket, int moduleID) : socket(socket), moduleID(moduleID), running(true) {
+    if (socket < 0) {
+        std::cerr << "Error: Pass a valid Socket\n";
         exit(EXIT_FAILURE);
     }
 
-    //Convert moduleID to hexadecimal before comparison
-    std::stringstream ss;
-    ss << std::hex << moduleID;
-    int moduleID_hex;
-    ss >> moduleID_hex;
     const int MIN_VALID_ID = 0x00000000;
     const int MAX_VALID_ID = 0x7FFFFFFF;
 
-    if (moduleID >= MIN_VALID_ID && moduleID <= MAX_VALID_ID) {
-        this->moduleID = moduleID;
-    } else{
-        std::cout << "Error: Pass a valid Module ID\n";
+    if (moduleID < MIN_VALID_ID || moduleID > MAX_VALID_ID) {
+        std::cerr << "Error: Pass a valid Module ID\n";
         exit(EXIT_FAILURE);
     }
+
+    // Print the moduleID for debugging
+    std::cout << "Module ID: 0x" << std::hex << std::setw(8) << std::setfill('0') << this->moduleID << std::endl;
 }
 
-bool ReceiveFrames::Receive(HandleFrames &handleFrame) {
-    struct can_frame frame;
-    int nbytes = read(this->socket, &frame, sizeof(struct can_frame));
-
-    // Check if the received frame is valid and complete
-    if (nbytes < 0) {
-        std::cerr << "Read error\n";
-        return false;
-    } else if (nbytes < sizeof(struct can_frame)) {
-        std::cerr << "Incomplete frame read\n";
-        return false;
-    }
-
-    // Check if the received frame is for your module
-    if (frame.can_id != this->moduleID) {
-        std::cerr << "Received frame is not for this module\n";
-        return false;
-    }
-
-    // Check if the received frame is empty
-    if (frame.can_dlc == 0) {
-        std::cerr << "Received empty frame\n";
-        return false;
-    }
-
-    // Print the frame for debugging
-    PrintFrame(frame);
-    
-    // Process the received frame
-    handleFrame.ProcessReceivedFrame(frame);
-
-    return true;
+ReceiveFrames::~ReceiveFrames() {
+    Stop();
 }
+
+void ReceiveFrames::Receive(HandleFrames &handleFrame) {
+    std::cout << "Starting Receive Method for Module ID: 0x" << std::hex << std::setw(8) << std::setfill('0') << this->moduleID << std::endl;
+    try {
+        producerThread = std::thread(&ReceiveFrames::Producer, this);
+        this->Consumer(handleFrame);
+    } catch (const std::exception &e) {
+        std::cerr << "Exception in starting threads: " << e.what() << std::endl;
+        Stop();
+    }
+}
+
+void ReceiveFrames::Stop() {
+    running = false;
+    cv.notify_all();
+    if (producerThread.joinable()) {
+        producerThread.join();
+    }
+}
+
+void ReceiveFrames::Producer() {
+    try{
+        while (running) {
+            struct can_frame frame;
+            int nbytes = read(this->socket, &frame, sizeof(struct can_frame));
+            if (nbytes < 0) {
+                // No data available or error occurred
+                if (errno == EWOULDBLOCK || errno == EAGAIN) {
+                    // No data available, wait for a short duration
+                    //std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+                    continue;
+                } else {
+                    // Other error occurred, handle it
+                    std::cerr << "Read error: " << strerror(errno) << std::endl;
+                    continue;
+                }
+            } else if (nbytes == 0) {
+                // Connection closed by peer
+                std::cerr << "Connection closed by peer" << std::endl;
+                continue;
+            }
+            
+            // Check if the received frame is for your module
+            if (frame.can_id != static_cast<canid_t>(this->moduleID)) {
+                std::cerr << "Received frame is not for this module\n";
+                continue;
+            }
+
+            // Check if the received frame is empty
+            if (frame.can_dlc == 0) {
+                std::cerr << "Received empty frame\n";
+                continue;
+            }
+            
+            // Print the frame for debugging
+            PrintFrame(frame);
+
+            std::unique_lock<std::mutex> lock(mtx);
+            frameBuffer.push(frame);
+            cv.notify_one();
+        }
+    }catch(const std::exception &e) {
+        std::cerr << "Exception in Producer: " << e.what() << std::endl;
+        Stop();
+    }
+}
+
+void ReceiveFrames::Consumer(HandleFrames &handleFrame) {
+    while (running) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]{ return !frameBuffer.empty() || !running; });
+        if (!running && frameBuffer.empty()) {
+            break;
+        }
+
+        struct can_frame frame = frameBuffer.front();
+        frameBuffer.pop();
+        lock.unlock();
+
+        // Process the received frame
+        handleFrame.ProcessReceivedFrame(frame);
+    }
+}
+
 
 void ReceiveFrames::PrintFrame(const struct can_frame &frame) {
     std::cout << "Received CAN frame:" << std::endl;
