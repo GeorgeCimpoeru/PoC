e138cb91e9929b0b3a884bf1b0111eb738bbcd0a
Mihnea Tanasevici <mihnea.tanasevici@randstaddigital.com>
2024-05-24 09:45:42 +0300

    added some UTs for receive and handle classes

---
+++ src/mcu/test/ReceiveFrames_test.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+#include <unistd.h>
+#include <linux/can.h>
+#include <sys/socket.h>
+#include <thread>
+#include <sstream>
+#include "../include/ReceiveFrames.h"
+
+class MockReceiveFrames : public ReceiveFrames {
+public:
+    MockReceiveFrames(int socket) : ReceiveFrames(socket) {}
+
+    // Expose private members for testing
+    using ReceiveFrames::ReceiveFramesFromCANBus;
+    using ReceiveFrames::ProcessQueue;
+    using ReceiveFrames::frameQueue;
+    using ReceiveFrames::queueMutex;
+    using ReceiveFrames::queueCondVar;
+    using ReceiveFrames::hexValueId;
+};
+
+class ReceiveFramesTest : public ::testing::Test {
+protected:
+    int mockSocket;
+    int mockSocketPair[2];
+    MockReceiveFrames* receiveFrames;
+
+    virtual void SetUp() {
+        // Create a mock socket pair for testing
+        socketpair(AF_UNIX, SOCK_STREAM, 0, mockSocketPair);
+        mockSocket = mockSocketPair[0];
+        receiveFrames = new MockReceiveFrames(mockSocket);
+    }
+
+    virtual void TearDown() {
+        delete receiveFrames;
+        close(mockSocketPair[0]);
+        close(mockSocketPair[1]);
+    }
+};
+
+TEST_F(ReceiveFramesTest, TestReceiveFramesFromCANBus_ReadError) {
+    close(mockSocket); // Close socket to simulate read error
+    int result = receiveFrames->ReceiveFramesFromCANBus();
+    EXPECT_EQ(result, 1);
+}
+
+TEST_F(ReceiveFramesTest, TestReceiveFramesFromCANBus_IncompleteFrameRead) {
+    std::thread readerThread([this] {
+        usleep(100);
+        struct can_frame frame;
+        frame.can_id = 0x123;
+        frame.can_dlc = 4;
+        write(mockSocketPair[1], &frame, sizeof(frame) - 1);
+    });
+
+    int result = receiveFrames->ReceiveFramesFromCANBus();
+    EXPECT_EQ(result, 1);
+    readerThread.join();
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
