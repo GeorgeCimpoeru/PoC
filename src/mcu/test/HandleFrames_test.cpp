6d3b734adb5586e5bb850e5335965c2ae56d9003
Mihnea Tanasevici <mihnea.tanasevici@randstaddigital.com>
2024-05-27 13:09:31 +0300

    refactored comments format

--- src/mcu/test/HandleFrames_test.cpp
+++ src/mcu/test/HandleFrames_test.cpp
@@ -11,46 +11,36 @@ class HandleFramesTest : public ::testing::Test{
 };
 
 TEST_F(HandleFramesTest, DiagnosticSessionControlTest){
+    testFrame.can_id = 0;      /* Set an arbitrary CAN ID */
+    testFrame.can_dlc = 2;     /* Set the Data Length Code to at least 2 to access data[1] */
     testFrame.data[1] = 0x10;
     testing::internal::CaptureStdout();
     handler.handleFrame(testFrame);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "DiagnosticSessionControl called.\n");
 }
-TEST_F(HandleFramesTest, DiagnosticSessionControlResponseTest){
-    testFrame.data[1] = 0x50;
-    testing::internal::CaptureStdout();
-    handler.handleFrame(testFrame);
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "DiagnosticSessionControl processed.\n");
-}
 
 TEST_F(HandleFramesTest, OtaRequestDownloadTest){
+    testFrame.can_id = 0;      /* Set an arbitrary CAN ID */
+    testFrame.can_dlc = 2;     /* Set the Data Length Code to at least 2 to access data[1] */
     testFrame.data[1] = 0x34;
     testing::internal::CaptureStdout();
     handler.handleFrame(testFrame);
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "Request download called.\n");
 }
-TEST_F(HandleFramesTest, OtaRequestDownloadResponseTest){
-    testFrame.data[1] = 0x74;
-    testing::internal::CaptureStdout();
-    handler.handleFrame(testFrame);
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "Request download processed.\n");
-}
-
 
 TEST_F(HandleFramesTest, UnknownFrameTest){
+    testFrame.can_id = 0;      /* Set an arbitrary CAN ID */
+    testFrame.can_dlc = 2;     /* Set the Data Length Code to at least 2 to access data[1] */
     testFrame.data[1] = 0x99;
     testing::internal::CaptureStderr();
     handler.handleFrame(testFrame);
     std::string output = testing::internal::GetCapturedStderr();
-    EXPECT_EQ(output, "Unknown service.\n");
-}
+    ASSERT_EQ(output, "Unknown service.\n");
+ }
 
-
-int main(int argc,char**argv)
+int main(int argc, char **argv)
 {
 testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
