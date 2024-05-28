89cb8e0a85f7235f6b6185e64924a5807e361e77
GeorgeCimpoeru <147798496+GeorgeCimpoeru@users.noreply.github.com>
2024-05-28 15:02:05 +0300

    Revert "Feature/ecu battery module"

--- src/ecu_simulation/BatteryModule/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/BatteryModule.h"
-
-int main() {
-    BatteryModule batteryModule;
-    batteryModule.simulate();
-    batteryModule.receiveFrames();
-    batteryModule.stopFrames();
-    return 0;
-}
