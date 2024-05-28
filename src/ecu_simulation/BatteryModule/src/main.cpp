893df3962e61f178b43ba664818e4261d254e886
TheoRandstad <theodor.stoica@randstaddigital.com>
2024-05-28 09:47:24 +0300

    added includes inside the header

--- src/ecu_simulation/BatteryModule/src/main.cpp
+++ src/ecu_simulation/BatteryModule/src/main.cpp
@@ -1,11 +1,7 @@
-#include <iostream>
-#include <thread>
 #include "../include/BatteryModule.h"
 
 int main() {
     BatteryModule batteryModule;
-    // BatteryModule batteryModule(1, 0x102);   // this will call the parameterized constructor of battery class
-                                                // and the interface-name will be 'vcan1', with moduleId '0x102' (258)
     batteryModule.simulate();
     batteryModule.receiveFrames();
     batteryModule.stopFrames();
