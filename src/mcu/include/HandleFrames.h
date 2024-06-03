a05815d01f34fcf548cfd978a6dcf97b3b605538
Mihnea Tanasevici <mihnea.tanasevici@randstaddigital.com>
2024-06-03 11:46:52 +0300

    added method to handle single/multi frames and another method to handle negative responses

--- src/mcu/include/HandleFrames.h
+++ src/mcu/include/HandleFrames.h
@@ -9,7 +9,7 @@
  * @author Tanasevici Mihnea
  * @date 2024-05-20
  * 
- */
+*/
 
 #ifndef HANDLE_FRAMES_H
 #define HANDLE_FRAMES_H
@@ -18,16 +18,37 @@
 #include <iostream>
 #include <vector>
 
-class HandleFrames {
+class HandleFrames 
+{
 public:
     /**
-     * @brief Method used to call a service or to handle a response from ECU.
-     * Takes a can_frame as parameter, parses the SID and calls the appropiate
-     * function within a switch-case.
+     * @brief Method used to handle a can frame received from the ReceiveFrame class.
+     * Takes a can_frame as parameter, checks if the frame is complete and then calls
+     * processFrameData() with either a single or multi frame.
      * 
      * @param[in] frame 
     */
     void handleFrame(const struct can_frame &frame);
+    /**
+     * @brief Method used to call a service or handle a response.
+     * It takes frame_id, service id(sid) and frame_data and calls the right service or
+     * handles the response received based on the given parameters.
+     * 
+     * @param[in] frame_id 
+     * @param[in] sid
+     * @param[in] frame_data
+     * @param[in] is_multi_frame
+    */
+    void processFrameData(canid_t frame_id, int sid, std::vector<uint8_t> frame_data, bool is_multi_frame);
+    /**
+     * @brief Method used to send a frame based on the nrc(negative response code) received.
+     * It takes as parameters frame_id, sid to identify the service, and nrc to send the correct
+     * negative response code back to who made the request.
+     * @param[in] frame_id 
+     * @param[in] sid 
+     * @param[in] nrc 
+     */
+    void processNrc(canid_t frame_id, int sid, int nrc);
 };
 
 #endif // HANDLE_FRAMES_H
--- src/mcu/src/HandleFrames.cpp
+++ src/mcu/src/HandleFrames.cpp
@@ -7,71 +7,449 @@
  */
 #include "../include/HandleFrames.h"
 
-/* Method to handle a can_frame*/
-void HandleFrames::handleFrame(const struct can_frame &frame) {
-    /* get frame id */
-    int frame_id = frame.can_id;
-    /* get data field */
-    std::vector<int> data(frame.data, frame.data + frame.can_dlc);
-    /* get Service ID */
-    int sid = data[1];
+/* Method to handle a can frame */
+void HandleFrames::handleFrame(const struct can_frame &frame) 
+{
+    /* Indicates whether the first frame has been received */
+    static bool first_frame_received = false;
+    /* Number of expected frames for multi-frame sequence */
+    static int expected_frames = 0;
+    /* frame data - for both single and multi frame sequence */
+    static std::vector<uint8_t> frame_data;
+    /* remember SID in case of multi-frame sequence*/
+    static int sid = 0;
+    /* check if the data is split across multiple frames */
+    static bool is_multi_frame = false;
+    /* Expected sequence number for consecutive frames */
+    static uint8_t expected_sequence_number = 0x21;
+
+    /* frame integrity checks will remain in Receive class for now */
+    /* id < 0x10 == single frame*/
+    if (frame.data[0] < 0x10) 
+    {
+        /* if frame is negative response, 0x7F takes sid's place so sid comes next to it */
+        if (frame.data[1] == 0x7F)
+        {
+            sid = frame.data[2];
+        }
+        else
+        {
+            sid = frame.data[1];  
+        }
+        std::cout << "Single Frame received:" << std::endl;
+        for (int i = 0; i < frame.can_dlc; ++i) 
+        {
+            frame_data.push_back(frame.data[i]);
+        }
+        std::cout << std::endl;
+        is_multi_frame = false;
+        /* Enter the switch case */
+        processFrameData(frame.can_id, sid, frame_data, is_multi_frame);
+        /* clear data to be used by a future frame */
+        frame_data.clear();
+    /* id == 0x10 == first frame */
+    } 
+    else if (frame.data[0] == 0x10) 
+    {
+        /* Number of expected frames */
+        expected_frames = frame.data[1] / 7;
+        if(frame.data[1] % 7 > 0)
+        {
+            expected_frames++;
+        }
+        /* get SID from the first frame */
+        sid = frame.data[2];
+        std::cout << "Multi-frame Sequence with " << expected_frames << " frames:" << std::endl;
+
+        /* Clear the multi_frame_data vector when receiving the first frame */
+        frame_data.clear();
+
+        /* Concatenate the data from the first frame into multi_frame_data */
+        for (int j = 5; j < frame.can_dlc; ++j) 
+        {
+            frame_data.push_back(frame.data[j]);
+        }
+        /* Make sure sequenceNumber is set after receiving the first frame */
+        expected_sequence_number = 0x21;
+        first_frame_received = true;
+    /* id >= 0x20 == consecutive frame */
+    } 
+    else if (frame.data[0] >= 0x21 && frame.data[0] < 0x30) 
+    {
+        if (!first_frame_received) 
+        {
+            /* Ignore consecutive frames until the first frame is received */
+            return;
+        }
+
+        /* Check if the frame's sequence number matches the expected sequence number */
+        if (frame.data[0] != expected_sequence_number) 
+        {
+            std::cerr << "Invalid consecutive frame sequence: expected " 
+                      << std::hex << int(expected_sequence_number) << " but received " 
+                      << std::hex << int(frame.data[0]) << std::endl;
+            return;
+        }
+        /* Concatenate the data from consecutive frames into multi_frame_data */
+        for (int j = 1; j < frame.can_dlc; ++j) 
+        {
+            frame_data.push_back(frame.data[j]);
+        }
+        /* Increment sequenceNumber after each concatenation*/
+        expected_sequence_number++;
+         /* Check if all multi-frames have been received */
+        if ((int)frame_data.size() >= expected_frames * 7) 
+        {
+            std::cout << "data is: ";
+            for (int j = 0; j < (int)frame_data.size(); ++j) 
+            {
+                std::cout << std::hex << int(frame_data[j]) << " ";
+            }
+            std::cout << std::endl;
+            is_multi_frame = true;
+            /* Enter the switch case */
+            processFrameData(frame.can_id, sid, frame_data, is_multi_frame);
+            /* Reset sequenceNumber for the next sequence */
+            expected_sequence_number = 0x21;
+            /* Reset variables */
+            first_frame_received = false;
+            expected_frames = 0;
+            frame_data.clear();
+        }
+    } 
+    else 
+    {
+        std::cerr << "Invalid frame type" << std::endl;
+    }
+}
+
+/* Method to call the service or handle the response*/
+void HandleFrames::processFrameData(canid_t frame_id, int sid, std::vector<uint8_t> frame_data, bool is_multi_frame) 
+{
 
     switch (sid) {
-    /* UDS Requests */
         case 0x10:
-            /* DiagnosticSessionControl(); */
-            std::cout << "DiagnosticSessionControl called." << std::endl;
+            /* DiagnosticSessionControl(sid, frame_data[2]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "DiagnosticSessionControl called." << std::endl;
+            }
             break;
         case 0x11:
-            /* EcuReset(); */
+            /* EcuReset(sid, frame_data[2]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "EcuReset called." << std::endl;
+            }
             break;
         case 0x27:
-            /* SecurityAccess(); */
+            /* SecurityAccess(sid, frame_data[2], key?); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "SecurityAccess called." << std::endl;
+            }
             break;
         case 0x29:
             /* Authentication(); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "Authentication called." << std::endl;
+            }
             break;
         case 0x3E:
-            /* TesterPresent(); */
+            /* TesterPresent(sid, frame_data[2]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "TesterPresent called." << std::endl;
+            }
             break;
         case 0x83:
-            /* AccessTimingParameters(); */
+            /* AccessTimingParameters(sid, frame_data[2]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "AccessTimingParameters called." << std::endl;
+            }
             break;
         case 0x22:
-            /* ReadDataByIdentifier(); */
+            /* ReadDataByIdentifier(sid, frame_data[2] << 8) | frame_data[3]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "ReadDataByIdentifier called." << std::endl;
+            }
             break;
         case 0x23:
-            /* ReadMemoryByAddress(); */
+            /* ReadMemoryByAddress(frame_data[2], frame_data[3] << 8) | frame_data[4], frame_data[5] << 8) | frame_data[6]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "ReadMemoryByAddress called." << std::endl;
+            }
             break;
         case 0x2E:
-            /* WriteDataByIdentifier(); */
+            /* WriteDataByIdentifier(sid, frame_data[2] << 8) | frame_data[3], data_parameter?); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else if (is_multi_frame)
+            {
+                std::cout << "WriteDataByIdentifier called with multiple frames." << std::endl;
+            }
+            else 
+            {
+                std::cout << "WriteDataByIdentifier called with one frame." << std::endl;
+            }
             break;
         case 0x14:
             /* ClearDiagnosticInformation(); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "ClearDiagnosticInformation called." << std::endl;
+            }
             break;
         case 0x19:
             /* ReadDtcInformation(); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "ReadDtcInformation called." << std::endl;
+            }
             break;
         case 0x31:
-            /* RoutineControl(); */
+            /* RoutineControl(sid, frame_data[2], frame_data[3] << 8) | frame_data[4]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "RoutineControl called." << std::endl;
+            }
             break;
-
+        /* UDS Responses */
+        case 0x50:
+            /* Response for DiagnosticSessionControl */
+            std::cout << "Response for DiagnosticSessionControl received." << std::endl;
+            break;
+        case 0x51:
+            /* Response from EcuReset() service */
+            std::cout << "Response from EcuReset received." << std::endl;
+            break;
+        case 0x67:
+            /* Response from SecurityAccess() service */
+            std::cout << "Response from SecurityAccess received." << std::endl;
+            break;
+        case 0x69:
+            /* Response from Authentication() service*/
+            std::cout << "Response from Authentication received." << std::endl;
+            break;
+        case 0x7E:
+            /* Response from TesterPresent() service */
+            std::cout << "Response from TesterPresent received." << std::endl;
+            break;
+        case 0xC3:
+            /* Response from AccessTimingParameters() service*/
+            std::cout << "Response from AccessTimingParameters received." << std::endl;
+            break;
+        case 0x62:
+            /* Response from ReadDataByIdentifier() service */
+            if(is_multi_frame)
+            {
+                std::cout << "Response from ReadDataByIdentifier received." << std::endl;
+                std::cout << "Received multiple frames containing " << frame_data.size() << " bytes of data" << std::endl;
+            }
+            else 
+            {
+                std::cout << "Response from ReadDataByIdentifier received in one frame." << std::endl;
+            }
+            break;
+        case 0x63:
+            /* Response from ReadMemoryByAddress() service */
+            if(is_multi_frame)
+            {
+                std::cout << "Response from ReadMemoryByAdress received." << std::endl;
+                std::cout << "Received multiple frames containing " << frame_data.size() << " bytes of data" << std::endl;
+            }
+            else 
+            {
+                std::cout << "Response from ReadMemoryByAdress received in one frame." << std::endl;
+            }
+            break;
+        case 0x6E:
+            /* Response from WriteDataByIdentifier() service */
+            std::cout << "Response from WriteDataByIdentifier received." << std::endl;
+            break;
+        case 0x54:
+            /* Response from ClearDiagnosticInformation() service */
+            std::cout << "Response from ClearDiagnosticInformation received." << std::endl;
+            break;
+        case 0x59:
+            /* Response from ReadDtcInformation() service */
+            std::cout << "Response from ReadDtcInformation received." << std::endl;
+            break;
+        case 0x71:
+            /* Response from RoutineControl() service */
+            std::cout << "Response from RoutineControl received." << std::endl;
+            break;
         /* OTA Requests */
         case 0x34:
-            /* RequestDownload(); */
-            std::cout << "Request download called." << std::endl;
+            /* RequestDownload(sid, frame_data[2], frame_data[3], frame_data[4], frame_data[5]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "RequestDownload called." << std::endl;
+            }
             break;
         case 0x36:
-            /* TransferData(); */
+            /* TransferData(sid, frame_data[2], frame_data[3], frame_data[4]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else if(is_multi_frame)
+            {
+                std::cout << "TransferData called with multiple frames." << std::endl;
+            }
+            else 
+            {
+                std::cout << "TransferData called with one frame" << std::endl;
+            }
             break;
         case 0x37:
-            /* RequestTransferExit(); */
+            /* RequestTransferExit(sid, frame_data[2]); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "RequestTransferExit called." << std::endl;
+            }
             break;
         case 0x32:
             /* RequestUpdateStatus(); */
+            if(frame_data[1] == 0x7F)
+            {
+                processNrc(frame_id, sid, (int)frame_data[3]);
+            }
+            else 
+            {
+                std::cout << "RequestUpdateStatus called." << std::endl;
+            }
+            break;
+        /* OTA Responses */
+        case 0x74:
+            /* Response from RequestDownload() service */
+            std::cout << "Response from RequestDownload received." << std::endl;
+            break;
+        case 0x76:
+            /*Response from TransferData() service */
+            std::cout << "Response from TransferData received." << std::endl;
+            break;
+        case 0x77:
+            /* Response from RequestTransferExit() service */
+            std::cout << "Response from RequestTransferExit received." << std::endl;
+            break;
+        case 0x72:
+            /* Response from RequestUpdateStatus() service */
+            std::cout << "Response from RequestUpdateStatus received." << std::endl;
             break;
         default:
-            std::cerr << "Unknown service." << std::endl;
+            /* Unknown request/response */
+            std::cout << "Unknown request/response received." << std::endl;
             break;
     }
-} 
+}
+void HandleFrames::processNrc(canid_t frame_id, int sid, int nrc)
+{
+    switch(nrc)
+    {
+        case 0x11:
+            /* Service not supported */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Service not supported for service: " << sid << std::endl;
+        break;
+        case 0x13:
+            /* Incorrect message length or invalid format */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Incorrect message length or invalid format for service: " << sid << std::endl;
+        break;
+        case 0x14:
+            /*  Response too long */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Response too long for service: " << sid << std::endl;
+        break;
+        case 0x25:
+            /* No response from subnet component */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: No response from subnet component for service: " << sid << std::endl;
+        break;
+        case 0x34:
+            /* Authentication failed */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Authentication failed for service: " << sid << std::endl;
+        break;
+        case 0x94:
+            /* Resource temporarily unavailable */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Resource temporarily unavailable for service: " << sid << std::endl;
+        break;
+        case 0x70:
+            /* Upload download not accepted */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Upload download not accepted for service: " << sid << std::endl;
+        break;
+        case 0x71:
+            /* Transfer data suspended */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Transfer data suspended for service: " << sid << std::endl;
+        break;
+        default:
+            /* Unknown negative response code */
+            //GenerateFrames::negativeResponse(can_id, sid, nrc);
+            std::cout << "Error: Unknown negative response code for service: " << sid << std::endl;
+        break;
+
+    }
+}
