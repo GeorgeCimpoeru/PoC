ebc4d8995b51a724622eef8d1371d8c421f48dae
Daniel Iancu <iancudaniel99@yahoo.com>
2024-06-05 09:06:49 +0300

    share a logger to all BatteryEcuModule files

---
+++ src/ecu_simulation/BatteryModule/include/BatteryModuleLogger.h
@@ -0,0 +1,9 @@
+#ifndef BATTERYMODULELOGGER_H
+#define BATTERYMODULELOGGER
+
+#include "../../../utils/Logger/include/Logger.h"
+
+/* Global logger for the Battery Module */
+extern Logger batteryModuleLogger;
+
+#endif
