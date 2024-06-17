1a52664f60b9c063825c1659c980051c61970ce2
Miruna-Maria <167984320+Miruna-Maria@users.noreply.github.com>
2024-06-17 09:02:13 +0300

    Feature/ecu battery module module (#65)
    * modified module id logic
    
    * optimized bufferFrameIn
    
    * changed name id and check complete id

--- src/ecu_simulation/BatteryModule/src/ReceiveFrames.cpp
+++ src/ecu_simulation/BatteryModule/src/ReceiveFrames.cpp
@@ -1,25 +1,39 @@
 #include "../include/ReceiveFrames.h"
 #include "../include/HandleFrames.h"
+#include "../include/BatteryModuleLogger.h"
 
-ReceiveFrames::ReceiveFrames(int socket, int module_id) : socket(socket), module_id(module_id), running(true) 
+Logger receiveModuleLogger = Logger("receiveFramesModule", "logs/receive_frames.log");
+
+ReceiveFrames::ReceiveFrames(int socket, int frame_id) : socket(socket), frame_id(frame_id), running(true) 
 {
     if (socket < 0) 
     {
-        std::cerr << "Error: Pass a valid Socket\n";
+        /* std::cerr << "Error: Pass a valid Socket\n"; */
+        LOG_INFO(receiveModuleLogger.getConsoleLogger(), "Error: Pass a valid Socket\n");
+        LOG_INFO(receiveModuleLogger.getFileLogger(), "Error: Pass a valid Socket\n");
         exit(EXIT_FAILURE);
     }
 
     const int MIN_VALID_ID = 0x00000000;
     const int MAX_VALID_ID = 0x7FFFFFFF;
 
-    if (module_id < MIN_VALID_ID || module_id > MAX_VALID_ID) 
+    if (frame_id < MIN_VALID_ID || frame_id > MAX_VALID_ID) 
     {
-        std::cerr << "Error: Pass a valid Module ID\n";
+        /* std::cerr << "Error: Pass a valid Module ID\n"; */
+        LOG_INFO(receiveModuleLogger.getConsoleLogger(), "Error: Pass a valid Module ID\n");
+        LOG_INFO(receiveModuleLogger.getFileLogger(), "Error: Pass a valid Module ID\n");
         exit(EXIT_FAILURE);
     }
 
-    /* Print the module_id for debugging */ 
-    std::cout << "Module ID: 0x" << std::hex << std::setw(8) << std::setfill('0') << this->module_id << std::endl;
+    /* Print the frame_id for debugging */ 
+    std::cout << "Module ID: 0x" << std::hex << std::setw(8) << std::setfill('0') << this->frame_id << std::endl;
+    LOG_INFO(receiveModuleLogger.getConsoleLogger(), 
+                "Module ID: {}", 
+                fmt::format("0x{:08X}", this->frame_id));
+                 
+    LOG_INFO(receiveModuleLogger.getFileLogger(), 
+                "Module ID: {}", 
+                fmt::format("0x{:08X}", this->frame_id));
 }
 
 ReceiveFrames::~ReceiveFrames() 
@@ -29,7 +43,7 @@ ReceiveFrames::~ReceiveFrames()
 
 void ReceiveFrames::receive(HandleFrames &handle_frame) 
 {
-    std::cout << "Starting receive Method for Module ID: 0x" << std::hex << std::setw(8) << std::setfill('0') << this->module_id << std::endl;
+    std::cout << "Starting receive Method for Module ID: 0x" << std::hex << std::setw(8) << std::setfill('0') << this->frame_id << std::endl;
     try 
     {
         bufferFrameInThread = std::thread(&ReceiveFrames::bufferFrameIn, this);
@@ -44,7 +58,7 @@ void ReceiveFrames::receive(HandleFrames &handle_frame)
     }
 }
 
-/* !!!!!!Set the socket to non-blocking mode. */
+/* Set the socket to non-blocking mode. */
 void ReceiveFrames::stop() 
 {   
     {
@@ -61,29 +75,47 @@ void ReceiveFrames::stop()
 void ReceiveFrames::bufferFrameIn() 
 {
     try{
+        /* Define a pollfd structure to monitor the socket */ 
+        struct pollfd pfd;
+        /* Set the socket file descriptor */ 
+        pfd.fd = this->socket; 
+        /* We are interested in read events -use POLLING */ 
+        pfd.events = POLLIN;    
+
         while (running) 
         {
-            struct can_frame frame = {};
-            int nbytes = 0;
-            while(running)
+            /* Use poll to wait for data to be available with a timeout of 1000ms (1 second) */ 
+            int poll_result = poll(&pfd, 1, 1000);
+
+            if (poll_result > 0) 
             {
-                nbytes = read(this->socket, &frame, sizeof(struct can_frame));
-                if(nbytes > 0)
+                if (pfd.revents & POLLIN) 
                 {
-                    break;
+                    struct can_frame frame = {};
+                    int nbytes = read(this->socket, &frame, sizeof(struct can_frame));
+
+                    if (nbytes > 0) 
+                    {
+                        std::unique_lock<std::mutex> lock(mtx);
+                        frame_buffer.push_back(std::make_tuple(frame, nbytes));
+                        cv.notify_one();
+                    }
                 }
-                sleep(1);
-            }
-            if (!running) {
+            } 
+            else if (poll_result == 0) 
+            {
+                /* Timeout occurred, no data available, but we just continue to check */ 
+                continue;
+            } 
+            else 
+            {
+                /* An error occurred */ 
+                std::cerr << "poll error: " << strerror(errno) << std::endl;
                 break;
             }
-            
-            std::unique_lock<std::mutex> lock(mtx);
-            frame_buffer.push_back(std::make_tuple(frame, nbytes));
-            cv.notify_one();
         }
     }
-    catch(const std::exception &e) 
+    catch (const std::exception &e) 
     {
         std::cerr << "Exception in bufferFrameIn: " << e.what() << std::endl;
         stop();
@@ -110,14 +142,31 @@ void ReceiveFrames::bufferFrameOut(HandleFrames &handle_frame)
         int nbytes = std::get<1>(frameTuple);
 
         /* Print the frame for debugging */ 
-            printFrame(frame);
+        printFrame(frame);
 
-         /* Check if the received frame is for your module */ 
-        if (static_cast<int>(frame.can_id) != module_id)
+        uint8_t frame_dest_id = frame.can_id & 0xFF;
+        current_module_id = frame_id & 0xFF;
+        /* Check if the received frame is for your module */ 
+        if (static_cast<int>(frame_dest_id) != current_module_id)
         {
             std::cerr << "Received frame is not for this module\n";
             continue;
         }
+
+        if (((frame.can_id >> 8) & 0xFF) == 0) 
+        {
+        std::cerr << "Invalid CAN ID: upper 8 bits are zero\n";
+        return;
+        }
+
+        /* Check if the frame is a request of type 'Up-Notification' from MCU */
+        if (frame_dest_id == 0x11 && frame.data[0] == 0x01)
+        {
+            notificationFlag = true;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+        
+        notificationFlag = false;
 
         /* Process the received frame */ 
         if (!handle_frame.checkReceivedFrame(nbytes, frame)) {
