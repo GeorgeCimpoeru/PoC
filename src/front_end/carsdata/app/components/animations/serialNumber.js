d55afce382b91a0cd44d2504ae3ce5856283d374
MihaelaDraga <mihaela.draga@randstaddigital.com>
2024-06-20 09:33:19 +0300

    fix pull request comments

--- src/front_end/carsdata/app/components/animations/serialNumber.js
+++ src/front_end/carsdata/app/components/animations/serialNumber.js
@@ -3,10 +3,10 @@ import "./style.css"
 function SerialNumber() {
       return (
             <lord-icon
-            src="https://cdn.lordicon.com/viawnmii.json"
-            trigger="hover"
-            class="my-lordicon">
-        </lord-icon>
+                  src="https://cdn.lordicon.com/viawnmii.json"
+                  trigger="hover"
+                  class="my-lordicon">
+            </lord-icon>
       );
 }
 
