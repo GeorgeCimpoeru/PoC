14d7a3ab0a292bc1a53c9ce2204218d794ddfbce
MihaelaDraga <mihaela.draga@randstaddigital.com>
2024-06-20 07:39:05 +0300

    added icons for doors module

--- src/front_end/carsdata/app/doorsModule/page.tsx
+++ src/front_end/carsdata/app/doorsModule/page.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
 import React from 'react'
+import SerialNumber from '../components/animations/serialNumber.js'
+import Car from '../components/animations/car.js'
+import LockUnlock from '../components/animations/lock.js'
+import Cigarette_light from '../components/animations/cigarette_light.js'
+import Indoor_light from '../components/animations/indoor_light.js'
 
 const DoorsModule = async () => {
     const res = await fetch(
@@ -52,6 +57,7 @@ const DoorsModule = async () => {
                                         <h2 className="card-title">Door</h2>
                                         <h1>Door position: {jsonData.query_params.door}</h1>
                                         <div className="card-actions justify-end">
+                                        <Car></Car>
                                         </div>
                                     </div>
                                 </div>
@@ -62,6 +68,7 @@ const DoorsModule = async () => {
                                         <h2 className="card-title">Door status</h2>
                                         <h1>Status: {jsonData.query_params.status}</h1>
                                         <div className="card-actions justify-end">
+                                        <Car></Car>
                                         </div>
                                     </div>
                                 </div>
@@ -72,6 +79,7 @@ const DoorsModule = async () => {
                                         <h2 className="card-title">Serial number</h2>
                                         <h1>Serial number: {jsonData.query_params.serial_number}</h1>
                                         <div className="card-actions justify-end">
+                                        <SerialNumber></SerialNumber>
                                         </div>
                                     </div>
                                 </div>
@@ -82,6 +90,31 @@ const DoorsModule = async () => {
                                         <h2 className="card-title">Cigarette lighter voltage</h2>
                                         <h1>Cigarette voltage: {jsonData.query_params.cigarette_lighter_voltage}</h1>
                                         <div className="card-actions justify-end">
+                                            <Cigarette_light></Cigarette_light>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card w-96 bg-green-300  text-black">
+                                    <div className="card-body">
+                                        <h2 className="card-title">Lock/Unlock state</h2>
+                                        <h1>Door state: {jsonData.query_params.door_state}</h1>
+                                        <div className="card-actions justify-end">
+                                        <LockUnlock></LockUnlock>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="card w-96 bg-green-300  text-black">
+                                    <div className="card-body">
+                                        <h2 className="card-title">Lights</h2>
+                                        <h1>Lights state: {jsonData.query_params.light_state}</h1>
+                                        <div className="card-actions justify-end">
+                                        <Indoor_light></Indoor_light>
                                         </div>
                                     </div>
                                 </div>
