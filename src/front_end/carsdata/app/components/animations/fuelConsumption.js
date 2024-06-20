ac98a17abae047535476c2ae36f4cdd423835ed9
andref00 <andreea.fertu@randstaddigital.com>
2024-06-20 13:41:57 +0300

    add animation and engine module

---
+++ src/front_end/carsdata/app/components/animations/fuelConsumption.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function FuelConsumption() {
+      return (
+        <lord-icon
+            src="https://cdn.lordicon.com/nohhjhod.json"
+            trigger="hover"
+            class="my-lordicon">
+        </lord-icon>
+      );
+}
+
+export default FuelConsumption
---
+++ src/front_end/carsdata/app/components/animations/serialNumber.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function SerialNumber() {
+      return (
+            <lord-icon
+                  src="https://cdn.lordicon.com/xfokkfex.json"
+                  trigger="hover"
+                  class="my-lordicon">
+            </lord-icon>
+      );
+}
+
+export default SerialNumber
