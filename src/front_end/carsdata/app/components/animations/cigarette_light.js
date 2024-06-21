e8fd89a79393546b15c5df1835c71eebfcf61855
DanielConstantinescu <daniel.constantinescu94@gmail.com>
2024-06-21 14:47:45 +0300

    [1] Frontend Sprint 4 (#98)
    * add the deleted code from DEV_FE (#61)
    
    * add the deleted code from DEV_FE
    
    * resolved conflicts
    
    * modified batteryModule
    
    * engine
    
    * Feature/front doors module (#82)
    
    * implement door module page
    
    * added icons for doors module
    
    * fix pull request comments
    
    * add animation and engine module
    
    * Feature/front Battery Module page (#83)
    
    * resolved conflicts
    
    * resolved requested changes
    
    ---------
    
    Co-authored-by: Elisei-26 <81551154+Elisei-26@users.noreply.github.com>
    Co-authored-by: amalia94 <amalia.giurgea@randstaddigital.com>
    Co-authored-by: andref00 <andreea.fertu@randstaddigital.com>
    Co-authored-by: MihaelaDraga <160007183+MihaelaDraga@users.noreply.github.com>
    Co-authored-by: andref00 <156180243+andref00@users.noreply.github.com>
    Co-authored-by: amalia94 <152972759+amalia94@users.noreply.github.com>

---
+++ src/front_end/carsdata/app/components/animations/cigarette_light.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function Cigarette_light() {
+    return (
+        <lord-icon
+            src="https://cdn.lordicon.com/zjppgibh.json"
+            trigger="hover"
+            class="my-lordicon">
+        </lord-icon>
+    );
+}
+
+export default Cigarette_light
