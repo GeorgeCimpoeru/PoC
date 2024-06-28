846c502f48b37113e293f85ff489597555247a19
amalia94 <amalia.giurgea@randstaddigital.com>
2024-06-28 12:17:45 +0300

    added new features for battery module cards

---
+++ src/front_end/carsdata/app/components/batteryComponents/RangeBatteryCard.tsx
@@ -0,0 +1,31 @@
+'use client'
+import React, { useState } from 'react'
+import Other from '../animations/other'
+
+
+const RangeBatteryCard = (props: any) => {
+      const [status, setstatus] = useState('off');
+      const change_status = () => {
+
+            if (status == 'on') {
+                  setstatus('off')
+            } else {
+                  setstatus('on');
+            }
+      }
+      
+      return (
+            <div className="card h-64 w-96 bg-green-300  text-black">
+                  <div className="card-body">
+                        <h2 className="card-title">Range battery</h2>
+                        <h1>Range: {status}</h1>
+                        <input type="checkbox" className="toggle" onChange={change_status} defaultChecked />
+                        <div className="card-actions justify-end">
+                              <Other></Other>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
+export default RangeBatteryCard
---
+++ src/front_end/carsdata/app/components/batteryComponents/VoltageCard.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React, { useState } from 'react'
+import BatteryVoltage from '../animations/batteryVoltage.js'
+import ModalString from '../sharedComponents/ModalString';
+
+const VoltageCard = (props: any) => {
+      const [inputValue, setInputValue] = useState(props.voltageCard);
+      const handleInputChange = (value: any, id: string) => {
+            setInputValue(value)
+
+      };
+
+      const handleInputClick = () => {
+            setInputValue('');
+      };
+      return (
+            <div className="card h-64 w-96 bg-green-300  text-black">
+                  <div className="card-body">
+                        <h2 className="card-title">Voltage</h2>
+                        <h1>Voltage: {inputValue}</h1>
+                        <label htmlFor="my_modal_5" className="w-10 btn btn-sm">Edit</label>
+                        <ModalString id="my_modal_5" handleInputChange={handleInputChange} handleInputClick={handleInputClick} cardTitle={'Battery state of charge'} />
+                        <div className="card-actions justify-end">
+                              <BatteryVoltage></BatteryVoltage>
+                        </div>
+                  </div>
+            </div>
+      )
+}
+
+export default VoltageCard
+
