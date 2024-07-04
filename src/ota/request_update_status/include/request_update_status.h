dd14a1fb06993e55e508dc3d47a93c1ba5142d29
Daniel Iancu <iancudaniel99@yahoo.com>
2024-07-04 09:32:52 +0300

    add comments and change some macros

--- src/ota/request_update_status/include/request_update_status.h
+++ src/ota/request_update_status/include/request_update_status.h
@@ -1,7 +1,16 @@
 /**
  * @file request_update_status.h
  * @author Iancu Daniel
- * @brief 
+ * @brief ***********************MCU Service. Requested from API***********************
+ * 			Request format: PCI_L(1byte) | SID(1byte)
+ * 			Response format(positive): PCI_L(1byte) | SSID(1byte) | OTA_DID_MSB(1byte) | OTA_DID_LSB(1byte) | OTA_UPDATE_STATUS(1byte)
+ * 			Response format(negative): pci_L(1byte) | NEGATIVE_RESPONSE(1byte) | SID(1byte) | NEGATIVE_RESPONSE_CODE(1byte)
+ * 
+ * 			The service uses ReadDataByIdentifier to get the value from Ota update status data identifier.
+ * 			ReadDataByIdentifier directly returns the response back to RequestUpdateStatus, without sending it on the can-bus.
+ * 			The OTA_UPDATE_STATUS_DID is in MCU memory.
+ * 
+ * 			The status is sent back to API from MCU.
  * @version 0.1
  * @date 2024-06-27
  * 
@@ -20,23 +29,31 @@
 #include "../../../utils/include/CreateInterface.h"
 #include "../../../utils/include/GenerateFrames.h"
 
-#define REQUEST_UPDATE_STATUS_SERVICE           0x32
-#define REQUEST_UPDATE_STATUS_SERVICE_SUCCESS   0x72
+#define REQUEST_UPDATE_STATUS_SID           0x32
+#define REQUEST_UPDATE_STATUS_SID_SUCCESS   0x72
 #define REQUEST_UPDATE_STATUS_REQUEST_SIZE      0x04
 #define REQUEST_UPDATE_STATUS_RESPONSE_SIZE	    0x01
 
 #define READ_DATA_BY_IDENTIFIER_SID 0x34
+#define READ_DATA_BY_IDENTIFIER_SID_SUCCESS 0x74
 
-#define OTA_STATUS_UPDATE_DID 0x01E0
-#define OTA_STATUS_UPDATE_DID_MSB ((OTA_STATUS_UPDATE_DID & 0xFF00) >> 8)
-#define OTA_STATUS_UPDATE_DID_LSB (OTA_STATUS_UPDATE_DID & 0x00FF)
+#define OTA_UPDATE_STATUS_DID 0x01E0
+#define OTA_UPDATE_STATUS_DID_MSB ((OTA_UPDATE_STATUS_DID & 0xFF00) >> 8)
+#define OTA_UPDATE_STATUS_DID_LSB (OTA_UPDATE_STATUS_DID & 0x00FF)
+
+#define PCI_L 0x03
+#define NEGATIVE_RESPONSE 0x7F
 
 #define SET_BYTE(n, i, b) (n = ((n & ~(0xFF << (i * 8))) | ((b & 0xFF) << (i * 8))))
 #define GET_BYTE(n, i) ((n >> (i * 8)) & 0xFF)
 
 
-
-enum class OtaUpdateStatesEnum
+/**
+ * @brief Enumeration with the Ota Update possible states. 
+ * 	MSN(nibble) represents the general state, LSN(nibble) represents the intermediary state, while a download/transfer/install is in progress.
+ * 
+ */
+typedef enum OtaUpdateStatesEnum
 {
 	IDLE = 0x00,        /* Initial state of the FOTA Handler after the ECU startup procedure */
     INIT = 0x10,        /* The FOTA Handler is initialized, and Dcm is set into the correct state (in Dcm FOTA session and security access has been granted) */
@@ -52,7 +69,7 @@ enum class OtaUpdateStatesEnum
     ACTIVATE_INSTALL_COMPLETE = 0x61,    /* FOTA installation has finished and received a respective service job from the FOTA Master that indicates the partition switch during the next boot process */
     ACTIVATE_INSTALL_FAILED = 0x62,    /* FOTA installation has finished and received a respective service job from the FOTA Master that indicates the partition switch during the next boot process */
 	ERROR = 0x70       /* Optional and implementer specific. Reserved state for e.g., implementer specific error handling, which is not (yet) covered by the FOTA Target */
-};
+}OtaUpdateStates;
 
 class RequestUpdateStatus
 {
@@ -62,7 +79,15 @@ private:
 public:
 	RequestUpdateStatus(Logger logger);
 	~RequestUpdateStatus();
-
+	/**
+	 * @brief Service method. Receive a request for reading Ota Update Status.
+	 * 	Sends a ReadDataByIdentifier request to MCU, with the Ota Update Status Data Identifier.
+	 * 	Gets the response from the service directly through return, not from can-bus.
+	 * 	Sends the response back to the initial sender.
+	 * 
+	 * @param[i] frame_id 
+	 * @param[i] frame_data 
+	 */
 	void requestUpdateStatus(canid_t frame_id, std::vector<uint8_t> frame_data);
 };
 
--- src/ota/request_update_status/src/request_update_status.cpp
+++ src/ota/request_update_status/src/request_update_status.cpp
@@ -13,6 +13,7 @@
 
 RequestUpdateStatus::RequestUpdateStatus(Logger logger) : _logger{logger}
 {
+    /* Interface needed for sending the response. */
     create_interface = create_interface->getInstance(0x00, _logger);
 }
 
@@ -32,33 +33,35 @@ void RequestUpdateStatus::requestUpdateStatus(canid_t request_id, std::vector<ui
     std::vector<uint8_t> readDataRequest;
     readDataRequest.emplace_back(request[0]);   /* PCI_l*/
     readDataRequest.emplace_back(READ_DATA_BY_IDENTIFIER_SID);   /* ReadDataByIdentifier SID */
-    readDataRequest.emplace_back(OTA_STATUS_UPDATE_DID_MSB);    /* OTA_UPDATE_STATUS_MSB_DID */
-    readDataRequest.emplace_back(OTA_STATUS_UPDATE_DID_LSB);    /* OTA_UPDATE_STATUS_LSB_DID */
+    readDataRequest.emplace_back(OTA_UPDATE_STATUS_DID_MSB);    /* OTA_UPDATE_STATUS_MSB_DID */
+    readDataRequest.emplace_back(OTA_UPDATE_STATUS_DID_LSB);    /* OTA_UPDATE_STATUS_LSB_DID */
 
     SET_BYTE(request_id, 1, 0x00); /* Set sender to 0x00, this will tell the readData service to not send a frame, but return the response */
     /* ReadDataByIdentifier RIDB; */ 
     /* std::vector<uint8_t> RIDB_response = RIDB.readDataByIdentifier(request_id, readDataRequest, _logger); */
 
     /* Dummy data, will be replaced by data from service */
-    std::vector<uint8_t> RIDB_response = {0x03, 0x7F, 0x01, 0xE0, 0x41};
-    std::vector<uint8_t> response;
+    std::vector<uint8_t> RIDB_response = {PCI_L, REQUEST_UPDATE_STATUS_SID_SUCCESS, OTA_UPDATE_STATUS_DID_MSB, OTA_UPDATE_STATUS_DID_LSB, OtaUpdateStatesEnum::IDLE};
+    std::vector<uint8_t> RUS_response;
 
-    if(RIDB_response[1] == 0x7F)
+    /* If we get a negative response from readDataByIdentifier service, send the same response from requestUpdateStatus, but with changed SID. */
+    if(RIDB_response[1] == NEGATIVE_RESPONSE)
     {
-        response.push_back(RIDB_response[0]);   /* PCI_l*/
-        response.push_back(RIDB_response[1]);   /* Negative response */
-        response.push_back(REQUEST_UPDATE_STATUS_SERVICE);  /* SID */
-        response.push_back(RIDB_response[3]); /* Negative response code */
+        RUS_response.push_back(RIDB_response[0]);           /* PCI_l*/
+        RUS_response.push_back(RIDB_response[1]);           /* Negative response */
+        RUS_response.push_back(REQUEST_UPDATE_STATUS_SID);  /* SID */
+        RUS_response.push_back(RIDB_response[3]);           /* Negative response code */
     }
     else
     {   
-        response.push_back(0x03);   /* PCI_l */
-        response.push_back(REQUEST_UPDATE_STATUS_SERVICE_SUCCESS); /* SERVICE SUCCESs = SID + 0X40 */
-        response.push_back(RIDB_response[4]);   /* OTA UPDATE STATUS */
+        /* Everything ok, Ota Update Status received*/
+        RUS_response.push_back(PCI_L);                              /* PCI_l */
+        RUS_response.push_back(REQUEST_UPDATE_STATUS_SID_SUCCESS);  /* SERVICE SUCCESs = SID + 0X40 */
+        RUS_response.push_back(RIDB_response[4]);                   /* OTA UPDATE STATUS */
     }
 
     GenerateFrames generate_frames(create_interface->getSocketApiWrite(), _logger);
-    generate_frames.sendFrame(response_id, response);
+    generate_frames.sendFrame(response_id, RUS_response);
 }
 
 
