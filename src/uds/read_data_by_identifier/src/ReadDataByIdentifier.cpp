e8b1b653e845a1a8b683466323e87116bfaa2dcb
Mihnea Tanasevici <mihnea.tanasevici@randstaddigital.com>
2024-07-04 12:09:27 +0300

    added ReadDataByIdentifier unit tests

--- src/uds/read_data_by_identifier/src/ReadDataByIdentifier.cpp
+++ src/uds/read_data_by_identifier/src/ReadDataByIdentifier.cpp
@@ -11,109 +11,96 @@ const uint8_t RDBI_SERVICE_ID = 0x22;
 /* Function to handle the Read Data By Identifier request */
 std::vector<uint8_t> ReadDataByIdentifier::readDataByIdentifier(canid_t can_id, const std::vector<uint8_t>& request, Logger& rdbi_logger) {
     std::vector<uint8_t> response;
-    /* Extract the data identifier from the request */
-    uint16_t data_identifier = (request[2] << 8) | request[3];
-        
-    /**Extract the first 8 bits of can_id */
+
+    /* Extract the first 8 bits of can_id */
     uint8_t lowerbits = can_id & 0xFF;
     uint8_t upperbits = can_id >> 8 & 0xFF;
+
+    /* Check if the request size is less than 4 */
+    if (request.size() < 4) {
+        /* Invalid request length - prepare a negative response */
+        response.push_back(0x03); /* PCI */
+        response.push_back(0x7F); /* Negative response */
+        response.push_back(RDBI_SERVICE_ID); /* Service ID */
+        response.push_back(0x13); /* Incorrect message length or invalid format */
+
+        /* Send the response frame to the bus */
+        create_interface = create_interface->getInstance(0x00, rdbi_logger);
+        if (create_interface) 
+        {
+            /* Send to API or canbus based on the upperbits(sender) */
+            generate_frames = new GenerateFrames(
+                (upperbits == 0xFA) ? create_interface->getSocketApiWrite() : create_interface->getSocketEcuWrite(),
+                rdbi_logger);
+                /* Send the negative response frame */ 
+                /* this will be replaced by NegativeResponse when done */
+                generate_frames->readDataByIdentifier(can_id, 0, response);
+        }
+        else
+        {
+            /* Handle the case where create_interface is null */
+            LOG_ERROR(rdbi_logger.GET_LOGGER(), "create_interface is nullptr");
+        }
+        /* Return early as the request is invalid */
+        return response;
+    }
+
+    /* Extract the data identifier from the request */
+    uint16_t data_identifier = (request[2] << 8) | request[3];
+
+    /* Reverse IDs */
+    can_id = ((lowerbits << 8) | upperbits);
 
     /* Vector to store data from ecuData */
     std::vector<uint8_t> stored_data;
 
-    /* Reverse ids */
-    can_id = ((lowerbits << 8) | upperbits);
-    if (request.size() < 3) 
-    {
-        /** Invalid request length */
-        response.push_back(0x03); /* PCI */
-        response.push_back(0x7F); /** Negative response */
-        response.push_back(RDBI_SERVICE_ID);
-        response.push_back(0x13); /** Incorrect message length or invalid format */
-    }
-    else
-    {
-        /** Determine which ECU data storage to use based on the first 8 bits of can_id */
-        if (lowerbits == 0x10 && MCU::mcu.mcu_data.find(data_identifier) != MCU::mcu.mcu_data.end()) 
-        {
+    /* Determine which ECU data storage to use based on the first 8 bits of can_id */
+    if ((lowerbits == 0x10 && MCU::mcu.mcu_data.find(data_identifier) != MCU::mcu.mcu_data.end()) ||
+        (lowerbits == 0x11 && battery.ecu_data.find(data_identifier) != battery.ecu_data.end())) {
+
+        /* Fetch the data based on the data identifier */
+        if (lowerbits == 0x10) {
             stored_data = MCU::mcu.mcu_data.at(data_identifier);
-            /* Positive response */
-            response.clear();
-            response.push_back(0x03); /* PCI */
-            response.push_back(0x62); /* Response sid = initial sid + 0x40 */
-            response.push_back(request[2]); /* Data identifier, two bytes */
-            response.push_back(request[3]);
-            /* Append the data corresponding to the identifier */
-            response.insert(response.end(), stored_data.begin(), stored_data.end());
-        } 
-        else if (lowerbits == 0x11 && battery.ecu_data.find(data_identifier) != battery.ecu_data.end()) 
-        {
-            /* fetch the data from linux */
+        } else {
             battery.fetchBatteryData();
             stored_data = battery.ecu_data.at(data_identifier);
-            /** Positive response */
-            response.clear();
-            response.push_back(0x03); /* PCI */
-            response.push_back(0x62); /* Response sid = initial sid + 0x40 */
-            response.push_back(request[2]); /* Data identifier, two bytes */
-            response.push_back(request[3]);
-            /** Append the data corresponding to the identifier */
-            response.insert(response.end(), stored_data.begin(), stored_data.end());
-        } 
-        else 
-        {
-            /** Data identifier not found */
-            /** response.push_back(0x03); */
-            /** response.push_back(0x7F); */ /** Negative response */
-            /** response.push_back(RDBI_SERVICE_ID); */
-            /** response.push_back(0x31); */ /** Request out of range */
+        }
 
-            /* 0x31 NRC is not yet defined so I log it instead */
-            LOG_ERROR(rdbi_logger.GET_LOGGER(), "Request out of range");
-        }
+        /* Prepare a positive response */
+        response.clear();
+        response.push_back(0x03); /* PCI */
+        response.push_back(0x62); /* Response SID = Initial SID + 0x40 */
+        response.push_back(request[2]); /* Data identifier, two bytes */
+        response.push_back(request[3]);
+        response.insert(response.end(), stored_data.begin(), stored_data.end());
+
+    } else {
+        /* Data identifier not found */
+        /* response.push_back(0x03); */
+        /* response.push_back(0x7F); */ /* Negative response */
+        /* response.push_back(RDBI_SERVICE_ID); */
+        /* response.push_back(0x31); */ /* Request out of range */
+        /* 0x31 NRC is not yet defined so I log it instead */
+        LOG_ERROR(rdbi_logger.GET_LOGGER(), "Request out of range");
     }
+
     /* Check if sender was API so MCU will send directly to API */
-    if (upperbits == 0xFA) 
-    {
-        create_interface = create_interface->getInstance(0x00, rdbi_logger);
-        if (create_interface) 
-        {   
-            generate_frames = new GenerateFrames(create_interface->getSocketApiWrite(), rdbi_logger);
-            if(response.size() > 8)
-            {
-                generate_frames->readDataByIdentifierLongResponse(can_id, data_identifier, response, true);
-            }
-            else
-            {
-                generate_frames->readDataByIdentifier(can_id, data_identifier, response);
-            }
-        } 
-        else 
-        {
-            /* Handle the case where create_interface is null */
-            LOG_ERROR(rdbi_logger.GET_LOGGER(), "create_interface is nullptr");
+    create_interface = create_interface->getInstance(0x00, rdbi_logger);
+    if (create_interface) {
+        generate_frames = new GenerateFrames(
+            (upperbits == 0xFA) ? create_interface->getSocketApiWrite() : create_interface->getSocketEcuWrite(),
+            rdbi_logger
+        );
+
+        if (response.size() > 8) {
+            generate_frames->readDataByIdentifierLongResponse(can_id, data_identifier, response, true);
+        } else {
+            generate_frames->readDataByIdentifier(can_id, data_identifier, response);
         }
-    } 
-    else 
-    {
-        create_interface = create_interface->getInstance(0x00, rdbi_logger);
-        if (create_interface) 
-        {
-            generate_frames = new GenerateFrames(create_interface->getSocketEcuWrite(), rdbi_logger);
-            if(response.size() > 8)
-            {
-                generate_frames->readDataByIdentifierLongResponse(can_id, data_identifier, response, true);
-            }
-            else
-            {
-                generate_frames->readDataByIdentifier(can_id, data_identifier, response);
-            }
-        } 
-        else 
-        {
-            /* Handle the case where create_interface is null */
-            LOG_ERROR(rdbi_logger.GET_LOGGER(), "create_interface is nullptr");
-        }
+    } else {
+        /* Handle the case where create_interface is null */
+        LOG_ERROR(rdbi_logger.GET_LOGGER(), "create_interface is nullptr");
     }
+
     return response;
 }
---
+++ src/uds/read_data_by_identifier/utest/ReadDataByIdentifierTest.cpp
@@ -0,0 +1,134 @@
+#include <gtest/gtest.h>
+#include "../include/ReadDataByIdentifier.h"
+#include "../../../ecu_simulation/BatteryModule/include/BatteryModule.h"
+#include "../../../mcu/include/MCUModule.h"
+
+class ReadDataByIdentifierTest : public ::testing::Test {
+protected:
+    Logger logger;
+    ReadDataByIdentifier rdbi;
+    CreateInterface* create_interface;
+    GenerateFrames generate;
+    /* Setup and teardown methods */
+    void SetUp() override {
+        /* Initialize any required data or state */
+        MCU::mcu.mcu_data.clear();
+        battery.ecu_data.clear();
+
+        /* Populate some test data for MCU */
+        MCU::mcu.mcu_data[0x1234] = {0x01, 0x02, 0x03, 0x04};
+
+        /* Populate some test data for Battery */
+        battery.ecu_data[0x5678] = {0x05, 0x06, 0x07, 0x08};
+    }
+
+    void TearDown() override {
+    }
+};
+
+TEST_F(ReadDataByIdentifierTest, ValidRequestForMCUData) {
+    canid_t can_id = 0x10; 
+    /* ReadDataByIdentifier request for data identifier 0x1234 */
+    std::vector<uint8_t> request = {0x22, 0x01, 0x12, 0x34};
+
+    std::vector<uint8_t> expected_response = {0x03, 0x62, 0x12, 0x34, 0x01, 0x02, 0x03, 0x04};
+    std::vector<uint8_t> actual_response = rdbi.readDataByIdentifier(can_id, request, logger);
+
+    EXPECT_EQ(expected_response, actual_response);
+}
+
+
+TEST_F(ReadDataByIdentifierTest, ValidRequestForBatteryData) {
+    canid_t can_id = 0x11;
+    /* ReadDataByIdentifier request for data identifier 0x5678 */
+    std::vector<uint8_t> request = {0x22, 0x01, 0x56, 0x78};
+
+    std::vector<uint8_t> expected_response = {0x03, 0x62, 0x56, 0x78, 0x05, 0x06, 0x07, 0x08};
+    std::vector<uint8_t> actual_response = rdbi.readDataByIdentifier(can_id, request, logger);
+
+    EXPECT_EQ(expected_response, actual_response);
+}
+
+TEST_F(ReadDataByIdentifierTest, InvalidRequestLength) {
+    canid_t can_id = 0x1000;
+    /* Invalid request (length < 4) */
+    std::vector<uint8_t> request = {0x22, 0x01};  
+
+    std::vector<uint8_t> expected_response = {0x03, 0x7F, 0x22, 0x13};
+    std::vector<uint8_t> actual_response = rdbi.readDataByIdentifier(can_id, request, logger);
+
+    EXPECT_EQ(expected_response, actual_response);
+}
+
+TEST_F(ReadDataByIdentifierTest, DataIdentifierNotFound) {
+    canid_t can_id = 0x1000;
+    /* Data identifier 0x9999 not in MCU data */
+    std::vector<uint8_t> request = {0x22, 0x01, 0x99, 0x99};
+
+    std::vector<uint8_t> expected_response = {};
+    EXPECT_NO_THROW({
+        std::vector<uint8_t> actual_response = rdbi.readDataByIdentifier(can_id, request, logger);
+        EXPECT_EQ(expected_response, actual_response);
+    });
+}
+
+TEST_F(ReadDataByIdentifierTest, CheckAPIFrameGeneration) {
+    canid_t can_id = 0xFA00;
+    std::vector<uint8_t> request = {0x22, 0x01, 0x12, 0x34};
+
+    EXPECT_NO_THROW({
+        rdbi.readDataByIdentifier(can_id, request, logger);
+    });
+}
+
+TEST_F(ReadDataByIdentifierTest, CheckNonAPIFrameGeneration) {
+    canid_t can_id = 0x1000;
+    std::vector<uint8_t> request = {0x22, 0x01, 0x12, 0x34};
+
+    EXPECT_NO_THROW({
+        rdbi.readDataByIdentifier(can_id, request, logger);
+    });
+}
+TEST_F(ReadDataByIdentifierTest, LongResponseForAPIFrame) {
+    canid_t can_id = 0xFA10;
+    std::vector<uint8_t> request = {0x22, 0x01, 0x12, 0x34};
+    /* 10 bytes of data */
+    std::vector<uint8_t> long_data(10, 0xBB);
+    MCU::mcu.mcu_data[0x1234] = long_data;
+
+    std::vector<uint8_t> actual_response = rdbi.readDataByIdentifier(can_id, request, logger);
+
+    std::vector<uint8_t> expected_response = {0x03, 0x62, 0x12, 0x34};
+    expected_response.insert(expected_response.end(), long_data.begin(), long_data.end());
+
+    EXPECT_EQ(expected_response.size(), actual_response.size());
+
+    for (size_t i = 0; i < expected_response.size(); ++i) {
+        EXPECT_EQ(expected_response[i], actual_response[i]) << "Mismatch at index " << i;
+    }
+
+    EXPECT_GT(actual_response.size(), 8);
+}
+
+TEST_F(ReadDataByIdentifierTest, LongResponseForNonAPIFrame) {
+    canid_t can_id = 0x1010;
+    std::vector<uint8_t> request = {0x22, 0x01, 0x12, 0x34};
+    create_interface = nullptr;
+    /* Populate MCU data with a long response */
+    std::vector<uint8_t> long_data(10, 0xBB);
+    MCU::mcu.mcu_data[0x1234] = long_data;
+
+    std::vector<uint8_t> actual_response = rdbi.readDataByIdentifier(can_id, request, logger);
+
+    std::vector<uint8_t> expected_response = {0x03, 0x62, 0x12, 0x34};
+    expected_response.insert(expected_response.end(), long_data.begin(), long_data.end());
+
+    EXPECT_EQ(expected_response.size(), actual_response.size());
+
+    for (size_t i = 0; i < expected_response.size(); ++i) {
+        EXPECT_EQ(expected_response[i], actual_response[i]) << "Mismatch at index " << i;
+    }
+
+    /* Assert that the size of the actual response is greater than 8 bytes */
+    EXPECT_GT(actual_response.size(), 8);
+}
