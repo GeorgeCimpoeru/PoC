6c40691eca95b782eff2368e37dc6b406f560af3
amalia94 <152972759+amalia94@users.noreply.github.com>
2024-07-05 13:34:13 +0300

    Started implementing OTA, edited battery module page (#131)
    * started implementing OTA, edited battery module page
    
    * changed tsconfig.json

--- src/front_end/carsdata/app/components/batteryComponents/FullyChargedCard.tsx
+++ src/front_end/carsdata/app/components/batteryComponents/FullyChargedCard.tsx
@@ -4,21 +4,20 @@ import BatteryFully from '../animations/batteryFully.js';
 
 const FullyChargedCard = (props: any) => {
     const [status, setstatus] = useState('off');
-    
+
     const change_status = () => {
         if (status == 'on') {
             setstatus('off');
         } else {
             setstatus('on');
         }
-    }
+    };
 
     return (
         <div className="card h-64 w-96 bg-green-300  text-black">
             <div className="card-body">
                 <h2 className="card-title">Fully charged</h2>
                 <h1>Fully charged: {status}</h1>
-                <input type="checkbox" className="toggle" onChange={change_status} defaultChecked />
                 <div className="card-actions justify-end">
                     <BatteryFully></BatteryFully>
                 </div>
--- src/front_end/carsdata/app/ota/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OTApage = () => {
-    return (
-        <div>OTApage</div>
-    )
-}
-
-export default OTApage
