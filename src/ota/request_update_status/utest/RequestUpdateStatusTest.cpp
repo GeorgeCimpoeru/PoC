ef312e9e7c7c7fa518e00de674dd7815ce44d819
Daniel Iancu <iancudaniel99@yahoo.com>
2024-07-10 16:11:42 +0300

    add unit tests and coverage for RequestUpdateStatus

---
+++ src/ota/request_update_status/utest/RequestUpdateStatusTest.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "../include/RequestUpdateStatus.h"
+#include "../../../mcu/include/MCUModule.h"
+
+class RequestUpdateStatusTest : public ::testing::Test {
+protected:
+    RequestUpdateStatus RUS;
+    /* Setup and teardown methods */
+    void SetUp() override
+    {
+
+    }
+
+    void TearDown() override 
+    {
+    
+    }
+};
+
+TEST_F(RequestUpdateStatusTest, RequestValid)
+{
+
+}
