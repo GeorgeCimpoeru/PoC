678f5f5972a45d578e3241a837ad748cc4476497
Daniel Iancu <iancudaniel99@yahoo.com>
2024-07-10 13:54:41 +0300

    write ota update did in the mcu constructor to IDLE

--- src/mcu/src/MCUModule.cpp
+++ src/mcu/src/MCUModule.cpp
@@ -15,7 +15,7 @@ namespace MCU
                     receive_frames(nullptr) 
                     {
         receive_frames = new ReceiveFrames(create_interface->getSocketEcuRead(), create_interface->getSocketApiRead());
-        WriteDataByIdentifier WDBI(0x1111FA10, {0x07, 0x2A, 0x01, 0xE0, 0x30}, MCULogger);
+        WriteDataByIdentifier WDBI(0x1111FA10, {PCI_L, WRITE_DATA_BY_IDENTIFIER_SID, OTA_UPDATE_STATUS_DID_MSB, OTA_UPDATE_STATUS_DID_LSB, IDLE}, MCULogger);
     }
 
     /* Default constructor */
