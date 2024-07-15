1d49a5a540d76fe69da0ec30b44972b82ab2d4ca
Elisei-26 <81551154+Elisei-26@users.noreply.github.com>
2024-07-15 11:59:12 +0300

    changed the theme and design of home page (#170)

--- src/front_end/carsdata/app/mainPage/page.tsx
+++ src/front_end/carsdata/app/mainPage/page.tsx
@@ -1,53 +1,19 @@
 'use client';
-import React, { useState } from 'react';
-import OpenMenu from '../components/mainPageComponents/OpenedMenu';
-import CloseMenu from '../components/mainPageComponents/ClosedMenu';
+import React from 'react';
+import LeftSideBar from '../components/mainPageComponents/LeftSideBar';
+import NavBar from '../components/mainPageComponents/NavBar';
 import Map from '../components/mainPageComponents/Map';
-import Divider from '../components/mainPageComponents/Divider';
-import RightSection from '../components/mainPageComponents/RightSection';
 
 const MainPage = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    const handleOutsideClick = (event: MouseEvent) => {
-        const target = event.target as HTMLElement;
-        if (!target.closest('#burgerMenu') && !target.closest('#dropdownMenu')) {
-            setMenuOpen(false);
-        }
-    };
-
-    React.useEffect(() => {
-        window.addEventListener('click', handleOutsideClick);
-        return () => {
-            window.removeEventListener('click', handleOutsideClick);
-        };
-    }, []);
-
+    let fruits: Array<string> = ['Apple', 'Orange', 'Banana'];
     return (
-        <div className="h-screen flex flex-col">
-            <nav className="bg-blue-600 p-8 flex justify-between items-center">
-                <div className="text-white text-2xl font-bold">carsData</div>
-                <div className="md:hidden">
-                    <button id="burgerMenu" className="text-white focus:outline-none" onClick={toggleMenu}>☰</button>
-                </div>
-                <OpenMenu />
-            </nav>
-            <input type="text" className="m-4 w-[20%]" placeholder="Search" />
-            <div className="flex w-full h-3/4 flex-col lg:flex-row">
+        <div className="flex w-full h-screen">
+            <LeftSideBar></LeftSideBar>
+            <div className="w-[90%] h-full flex flex-col">
+                <NavBar></NavBar>
                 <Map></Map>
-                <Divider></Divider>
-                <RightSection></RightSection>
             </div>
-
-            {menuOpen && (
-                <CloseMenu />
-            )}
         </div>
-      
     )
 }
 
