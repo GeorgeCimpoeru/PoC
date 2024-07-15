66a788b90b13ed1c16e370e7f80db37c071ece73
lodug <loredana.dugulean@gmail.com>
2024-07-15 10:53:15 +0300

    minor updates due to comments

--- src/ota/request_transfer_exit/utest/RequestTransferExitTest.cpp
+++ src/ota/request_transfer_exit/utest/RequestTransferExitTest.cpp
@@ -6,18 +6,18 @@ class RequestTransferExitTest : public ::testing::Test
 {
 protected:
     Logger logger;
-    RequestTransferExit* transferExit;
+    RequestTransferExit* transfer_exit;
     RequestTransferExitTest()
     {
-    transferExit = new RequestTransferExit(logger);
+    transfer_exit = new RequestTransferExit(logger);
     }
 };
 
 TEST_F(RequestTransferExitTest, TransferSuccesNoCallbackSet)
 {
     /* simulate a successfull transfer data */
-    bool transferSuccess =  true;
-    bool result = transferExit->requestTransferExit(0x10, transferSuccess);
+    bool transfer_success =  true;
+    bool result = transfer_exit->requestTransferExit(0x10, transfer_success);
     /* should stop as no callback is set */
     EXPECT_FALSE(result);
 }
@@ -25,8 +25,8 @@ TEST_F(RequestTransferExitTest, TransferSuccesNoCallbackSet)
 TEST_F(RequestTransferExitTest, TransferFailNoCallbackSet)
 {
     /* simulate a failed transfer data*/
-    bool transferSuccess =  false;
-    bool result = transferExit->requestTransferExit(0x10, transferSuccess);
+    bool transfer_success =  false;
+    bool result = transfer_exit->requestTransferExit(0x10, transfer_success);
     /* should stop as no callback is set */
     EXPECT_FALSE(result);
 }
@@ -34,36 +34,36 @@ TEST_F(RequestTransferExitTest, TransferFailNoCallbackSet)
 TEST_F(RequestTransferExitTest, TransferSuccesCallbackSet)
 {
     /* simulate a successfull transfer data*/
-    bool transferSuccess =  true;
+    bool transfer_success =  true;
     /** Set a lambda function as the callback for the RequestTransferExit class that takes 
     *   one parameter of type boolean named and the body of lambda simply returns the success parameter
     */
-    transferExit->setTransferCompleteCallBack([](bool success) { return success; });
+    transfer_exit->setTransferCompleteCallBack([](bool success) { return success; });
 
-    bool result = transferExit->requestTransferExit(0x10, transferSuccess);
-    /*  */
+    bool result = transfer_exit->requestTransferExit(0x10, transfer_success);
+
     EXPECT_TRUE(result);
 }
 
 TEST_F(RequestTransferExitTest, TransferFailCallbackSet)
 {
    /* simulate a failed transfer data*/
-    bool transferSuccess =  false;
-    transferExit->setTransferCompleteCallBack([](bool success) { return success; });
+    bool transfer_success =  false;
+    transfer_exit->setTransferCompleteCallBack([](bool success) { return success; });
 
-    bool result = transferExit->requestTransferExit(0x10, transferSuccess);
-    /*  */
+    bool result = transfer_exit->requestTransferExit(0x10, transfer_success);
+
     EXPECT_FALSE(result); 
 }
 
 TEST_F(RequestTransferExitTest, CallbackNullptr)
 {
     /* simulate a successfull transfer data*/
-    bool transferSuccess =  true;
+    bool transfer_success =  true;
     /* Explicitly set the callback nullptr */
-    transferExit->setTransferCompleteCallBack(nullptr);
+    transfer_exit->setTransferCompleteCallBack(nullptr);
 
-    bool result = transferExit->requestTransferExit(0x10, transferSuccess);
+    bool result = transfer_exit->requestTransferExit(0x10, transfer_success);
     /* should stop the transfer as callback is nullptr */
     EXPECT_FALSE(result); 
 }
