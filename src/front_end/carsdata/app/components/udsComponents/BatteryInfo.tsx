f9f68c65e2e5ea87a9315e29029749d113adf840
Elisei-26 <pralaelisei76@gmail.com>
2024-07-22 09:33:05 +0300

    api configuration for UDS

--- src/front_end/carsdata/app/components/udsComponents/BatteryInfo.tsx
+++ src/front_end/carsdata/app/components/udsComponents/BatteryInfo.tsx
@@ -1,47 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const BatteryInfo = () => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+const BatteryInfo = async () => {
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://127.0.0.1:5000/api/read_info_battery', {
-                    cache: 'no-store'
-                });
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const result = await response.json();
-                console.log(result)
-                setData(result);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
-        };
+    // const [data, setData] = useState(null);
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(null);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch('http://127.0.0.1:5000/api/read_info_battery', {
+    //                 cache: 'no-store'
+    //             });
+    //             if (!response.ok) {
+    //                 throw new Error('Network response was not ok');
+    //             }
+    //             const result = await response.json();
+    //             console.log(result)
+    //             setData(result);
+    //         } catch (error) {
+    //             setError(error);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+    //     fetchData();
+    // }, []); // Empty dependency array ensures this runs once after the initial render
+    // if (loading) {
+    //     return <div>Loading...</div>;
+    // }
+    // if (error) {
+    //     return <div>Error: {error.message}</div>;
+    // }
+    // if (data) {
+    //     console.log(data);
+    // }
+    // if (!data) {
+    //     return <div>No data available</div>;
+    // }
 
-        fetchData();
-    }, []); // Empty dependency array ensures this runs once after the initial render
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    const res = await fetch(
+        'https://api.agify.io/?name=meelad',
+        { cache: 'no-store' }
+    );
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+    const jsonData = await res.json();
 
-    if (data) {
-        console.log(data);
-    }
-
-    if (!data) {
-        return <div>No data available</div>;
-    }
+    // const response = await fetch('https://api.agify.io/?name=meelad', {
+    //     cache: 'no-store'
+    // });
+    // const result = await response.json();
 
     return (
         <div className="overflow-x-auto">
@@ -54,7 +62,14 @@ const BatteryInfo = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {Object.entries(data).map(([key, value]) => (
+                    {jsonData.stringify}
+                    {/* {result.item.map((key: any, value: any) => (
+                        <tr key={key}>
+                            <td>{key}</td>
+                            <td>{value}</td>
+                        </tr>
+                    ))} */}
+                    {Object.entries(jsonData).map(([key, value]) => (
                         <tr key={key}>
                             <td>{key}</td>
                             <td>{value}</td>
