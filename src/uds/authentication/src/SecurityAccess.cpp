099dbc02133069635db904e299518b3dc9585cf5
TheoRandstad <theodor.stoica@randstaddigital.com>
2024-07-22 14:05:30 +0300

    updated random length seed + cast correctly

--- src/uds/authentication/src/SecurityAccess.cpp
+++ src/uds/authentication/src/SecurityAccess.cpp
@@ -135,12 +135,12 @@ void SecurityAccess::securityAccess(canid_t can_id, const std::vector<uint8_t>& 
             {
                 /* Adjust the seed length between 1 and 5.*/
                 std::srand(static_cast<unsigned>(std::time(nullptr)));
-                size_t seed_length = 3;
+                size_t seed_length = std::rand() % 5 + 1;
 
                 std::vector<uint8_t> seed = generateRandomBytes(seed_length);
 
                 /* PCI length = seed_length + 2(0x67 and 0x01)*/
-                response.push_back(2 + seed_length);
+                response.push_back(static_cast<uint8_t>(2 + seed_length));
 
                 /* SID response */
                 response.push_back(0x67);
