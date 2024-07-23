4f138dd19b3b8f308fa3a0436057da2ee4779230
lodug <loredana.dugulean@gmail.com>
2024-07-23 14:58:25 +0300

    unit tests for request download

--- src/ota/request_download/utest/RequestDownloadTest.cpp
+++ src/ota/request_download/utest/RequestDownloadTest.cpp
@@ -7,11 +7,10 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 
-class RequestDownloadTest : public ::testing::Test 
+class RequestDownloadTest : public ::testing::Test
 {
 protected:
-    Logger logger;
-    //int socket;
+    Logger logger;    
     GenerateFrames* generate;
     DiagnosticSessionControl diagnostic_session;
     ReadDataByIdentifier software_version;
@@ -28,25 +27,21 @@ protected:
     }
 
     /* Setup and teardown methods */
-    void SetUp() override 
-    { 
-        //socket = 8;
-        generate = new GenerateFrames(8, logger);      
+    void SetUp() override
+    {
+        generate = new GenerateFrames(8, logger);
     }
 
-    void TearDown() override 
+    void TearDown() override
     {
         delete generate;
     }
 };
 
 TEST_F(RequestDownloadTest, CompressionType)
-{      
-    //ASSERT_NE(generate, nullptr);
-    //generate->requestDownload(0x111110,0x0,0x12,0x12,0x88);
-    //int id = 0x10FA10;
-    int id = 0x111110;    
-    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x12, 0x12, 0x88 };    
+{
+    int id = 0x111110;
+    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x12, 0x12, 0x88 };
     /* redirect stdout to capture the output */
     testing::internal::CaptureStdout();
     request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
@@ -57,12 +52,9 @@ TEST_F(RequestDownloadTest, CompressionType)
 }
 
 TEST_F(RequestDownloadTest, EncryptionType)
-{      
-    //ASSERT_NE(generate, nullptr);
-    //generate->requestDownload(0x111110,0x0,0x12,0x12,0x88);
-    //int id = 0x10FA10;
-    int id = 0x111110;    
-    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x12, 0x12, 0x88 };    
+{
+    int id = 0x111110;
+    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x12, 0x12, 0x88 };
     /* redirect stdout to capture the output */
     testing::internal::CaptureStdout();
     request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
@@ -74,85 +66,85 @@ TEST_F(RequestDownloadTest, EncryptionType)
 
 TEST_F(RequestDownloadTest, ValidMemoryRange)
 {
-    //ASSERT_NE(generate, nullptr);
     int id = 0x101110;  
-    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x12, 0x12, 0x88 };
+    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x11, 0x12, 0x12, 0x88 };
     /* redirect stdout to capture the output */
     testing::internal::CaptureStdout();
-    //generate->requestDownload(0x111110,0x0,0x12,0x12,0x88);
     request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
     std::string output = testing::internal::GetCapturedStdout();
-    // int id = 0x101110; 
-    // std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x01, 0x12, 0x12, 0x88 };
-    
-            
-    /* redirect stdout to capture the output */
-    // testing::internal::CaptureStdout();
-    // request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
-    // std::string output = testing::internal::GetCapturedStdout();
     /* check if the correct message is printed */   
     std::string message = "Validated Memory Address: 0x12";
-    std::string message1 = "Validated Memory Size: 0x8800";
+    std::string message1 = "Validated Memory Size: 0x12";
+    EXPECT_NE(output.find(message), std::string::npos);
+    EXPECT_NE(output.find(message1), std::string::npos);
+}
+
+TEST_F(RequestDownloadTest, InvalidMemoryAddress)
+{
+    int id = 0x101110;
+    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x51, 0x12, 0xFF, 0xFF, 0xFF, 0xFF, 0x12, 0x88 };
+    /* redirect stdout to capture the output */
+    testing::internal::CaptureStdout();
+    request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
+    std::string output = testing::internal::GetCapturedStdout();    
+    /* check if the correct message is printed */  
+    std::string message = "Error: Invalid memory address:";
+    std::string message1 = "Error: Invalid memory range";
     EXPECT_NE(output.find(message), std::string::npos);
     EXPECT_NE(output.find(message1), std::string::npos);
 }
 
 TEST_F(RequestDownloadTest, InvalidMemorySize)
 {
-    //ASSERT_NE(generate, nullptr);
     int id = 0x101110;
-    //std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x11, 0xFF, 0x00, 0x88 };
-    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x21, 0x12, 0x34, 0x00, 0x88 };
+    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x15, 0x12, 0XFF, 0XFF, 0XFF, 0XFF, 0xFF, 0x88 };
     /* redirect stdout to capture the output */
-    testing::internal::CaptureStderr();
-    //generate->requestDownload(0x111110,0x0,0x12,0x12,0x88);
+    testing::internal::CaptureStdout();
     request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
-    std::string output = testing::internal::GetCapturedStderr();    
+    std::string output = testing::internal::GetCapturedStdout();    
     /* check if the correct message is printed */  
-    std::string message = "Error: Invalid memory size: 0";
-    std::string output1 = testing::internal::GetCapturedStderr(); 
+    std::string message = "Error: Invalid memory size:";    
     std::string message1 = "Error: Invalid memory range";    
     EXPECT_NE(output.find(message), std::string::npos);
-    EXPECT_NE(output1.find(message1), std::string::npos);
+    EXPECT_NE(output.find(message1), std::string::npos);
 }
 
-// TEST_F(RequestDownloadTest, InvalidMemoryAddressRange)
-// {
-//     //ASSERT_NE(generate, nullptr);
-//     int id = 0x101110;  
-//     std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x21, 0x01, 0x00, 0x01, 0x88 };
-//     /* redirect stdout to capture the output */
-//     testing::internal::CaptureStderr();
-//     //generate->requestDownload(0x111110,0x0,0x12,0x12,0x88);
-//     request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
-//     std::string output = testing::internal::GetCapturedStderr();    
-//     /* check if the correct message is printed */  
-//     std::string message = "Error: Invalid memory address: 0"; 
-//     std::string output1 = testing::internal::GetCapturedStderr();
-//     std::string message1 = "Error: Invalid memory range";    
-//     EXPECT_NE(output.find(message), std::string::npos);
-//     EXPECT_NE(output.find(message1), std::string::npos);
-// }
+TEST_F(RequestDownloadTest, RequestDownloadResp)
+{
+    int id = 0x101110; 
+    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x11, 0x12, 0x12, 0x88 };
+    int max_number_block = 0;
+    /* redirect stdout to capture the output */
+    testing::internal::CaptureStdout();
+    request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
+    std::string output = testing::internal::GetCapturedStdout();
+    /* check if the correct message is printed */   
+    std::string message = "frame id dest: 0x10";
+    std::string message1 = "log in service";
+    std::string message2 = "frame id";
+    std::string message3 = "max no block";
+    EXPECT_NE(output.find(message), std::string::npos);
+    EXPECT_NE(output.find(message1), std::string::npos);
+    EXPECT_NE(output.find(message2), std::string::npos);
+    EXPECT_NE(output.find(message3), std::string::npos);
+}
 
-// TEST_F(RequestDownloadTest, RequestDownloadResp)
-// {
-//     int id = 0x101110; 
-//     std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x12, 0x12, 0x88 };
-//     int max_number_block = 0;
-//     /* redirect stdout to capture the output */
-//     testing::internal::CaptureStdout();
-//     request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
-//     std::string output = testing::internal::GetCapturedStdout();
-//     /* check if the correct message is printed */   
-//     std::string message = " frame id dest: 0x}";
-//     EXPECT_EQ(output, message );
-// }
+TEST_F(RequestDownloadTest, RequestDownloadRespElse)
+{
+    int id = 0x111111; 
+    std::vector<uint8_t> stored_data = { 0x07, 0x34, 0x00, 0x11, 0x12, 0x12, 0x88 };
+    int max_number_block = 0;
+    /* redirect stdout to capture the output */
+    testing::internal::CaptureStderr();
+    request_download.requestDownloadRequest(id, stored_data, logger, diagnostic_session, software_version, logged_in);
+    std::string output = testing::internal::GetCapturedStderr();
+    /* check if the correct message is printed */   
+    std::string message = "Create interface is nullptr";
+    EXPECT_NE(output.find(message), std::string::npos);
+}   
 
 TEST_F(RequestDownloadTest, DonwloadFromDrivePart3If)
-{       
-    //ASSERT_NE(generate, nullptr);
-    //generate->requestDownload(0x101110,0x0,0x12,0x12,0x89);
-    //int id = 0x10FA10;
+{           
     int id = 0x101110;
     std::vector<uint8_t> stored_data = { 0x5, 0x34, 0x00, 0x11, 0x12, 0x12, 0x89 };
     /* redirect stdout to capture the output */
@@ -165,10 +157,7 @@ TEST_F(RequestDownloadTest, DonwloadFromDrivePart3If)
 }
 
 TEST_F(RequestDownloadTest, DonwloadFromDrivePart3else)
-{       
-    //ASSERT_NE(generate, nullptr);
-    //generate->requestDownload(0x111110,0x0,0x12,0x12,0x89);
-    //int id = 0x10FA10;
+{
     int id = 0x111110;
     std::vector<uint8_t> stored_data = { 0x5, 0x34, 0x00, 0x11, 0x12, 0x12, 0x89 };
     /* redirect stdout to capture the output */
