f87a492da11a776ab3d1ac548e88a172dd24c0ed
amalia94 <152972759+amalia94@users.noreply.github.com>
2024-07-31 14:55:12 +0300

    Feature/ota version control (#235)
    * added new features for OTA page
    
    * changed OTAcomponenents
    
    * added new features on ota_version_control
    
    * ota version control unfinished

--- src/front_end/carsdata/app/components/OTAcomponents/LeftSideBar.tsx
+++ src/front_end/carsdata/app/components/OTAcomponents/LeftSideBar.tsx
@@ -1,31 +1,50 @@
 import React, { useState } from 'react';
 import Image from "next/image";
+import TableOta from './TableOta';
 
 const LeftSideBar = (props: any) => {
     const [activeIcon, setActiveIcon] = useState<number | null>(null);
+    const [hoveredIcon, setHoveredIcon] = useState<number | null>(null);
 
     const handleIconClick = (iconIndex: number) => {
         setActiveIcon(iconIndex);
         props.clickedIcon(iconIndex);
     };
 
+    const handleMouseEnter = (iconIndex: number) => {
+        setHoveredIcon(iconIndex);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIcon(null);
+    };
+
     return (
         <div className="w-[6%] bg-blue-700 rounded-lg m-5 p-6 shadow-xl flex flex-col items-center">
             <div
                 onClick={() => handleIconClick(1)}
+                onMouseEnter={() => handleMouseEnter(1)}
+                onMouseLeave={handleMouseLeave}
                 className={activeIcon === 1 ? 'bg-white/50 p-1 rounded-lg' : ''}
             >
                 <Image
-                    src="/cloud.png"
-                    alt="Cloud icon"
+                    src="/ECU.png"
+                    alt="ECU icon"
                     className="dark:invert m-1 hover:object-scale-down"
                     width={100}
                     height={24}
                     priority
                 />
+                {hoveredIcon === 1 && (
+                    <div className="absolute bg-gray-700 text-white p-2 rounded mt-1">
+                        ECU
+                    </div>
+                )}
             </div>
             <div
                 onClick={() => handleIconClick(2)}
+                onMouseEnter={() => handleMouseEnter(2)}
+                onMouseLeave={handleMouseLeave}
                 className={activeIcon === 2 ? 'bg-white/50 p-1 rounded-lg' : ''}
             >
                 <Image
@@ -36,6 +55,11 @@ const LeftSideBar = (props: any) => {
                     height={24}
                     priority
                 />
+                {hoveredIcon === 2 && (
+                    <div className="absolute bg-gray-700 text-white p-2 rounded mt-1">
+                        History
+                    </div>
+                )}
             </div>
         </div>
     );
--- src/front_end/carsdata/app/components/OTAcomponents/TableOta.tsx
+++ src/front_end/carsdata/app/components/OTAcomponents/TableOta.tsx
@@ -24,11 +24,12 @@ const TableOta = (props: any) => {
                             <td align="center">Completed</td>
                             <td align="center">Failed</td>
                             <td align="center"> <label htmlFor="my_modal_1" className="w-10 btn btn-sm">Log info</label></td>
-                            <ModalString id="my_modal_1" cardTitle={'Door'} />
+                            <ModalString id="my_modal_1" cardTitle={'Log info'} />
                         </tr>
                     ))}
                 </tbody>
             </table>
+
             <div className="absolute right-0 top-0 m-5">
             </div>
         </div>
