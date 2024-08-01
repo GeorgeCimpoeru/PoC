36d4055ffe0826c33a2cda40d49af68be3734979
lodug <153726063+lodug@users.noreply.github.com>
2024-08-01 11:07:46 +0300

    update the files for transfer exit with a new method (#230)
    * update the files for transfer exit with a new method
    
    * updated the code using method from generate frames for the negateive response
    
    * deleted an unnecessary comment

--- src/ota/request_transfer_exit/src/RequestTransferExit.cpp
+++ src/ota/request_transfer_exit/src/RequestTransferExit.cpp
@@ -8,16 +8,21 @@
 
 #include "../include/RequestTransferExit.h"
 #include <iostream>
+#include "../../../mcu/include/MCUModule.h"
 
 /* Constructor that initializes the callback_ to nullptr */
-RequestTransferExit::RequestTransferExit(Logger& RTESLogger) : callback_(nullptr), RTESLogger(RTESLogger)
+RequestTransferExit::RequestTransferExit(int socket, Logger& RTESLogger) : callback_(nullptr), RTESLogger(RTESLogger), generate_frames(socket, RTESLogger)
 {
+    this->socket = socket;
 }
 
 /* Destructor */
 RequestTransferExit::~RequestTransferExit()
 {    
 }
+
+/* Define the service identifier for Read Data By Identifier */
+const uint8_t RTES_SERVICE_ID = 0x37;
 
 /* Method to set the callback function */
 void RequestTransferExit::setTransferCompleteCallBack(transferCompleteCallBack callback)
@@ -44,4 +49,51 @@ bool RequestTransferExit::requestTransferExit(int id, bool transferSuccess)
 
     /* If no callback is set, default to false to stop the transfer */
     return false;
+}
+
+/* Method to handle the request transfer exit 0x37 */
+/* frame format = {PCI_L, SID(0x37), transfer_request_parameter_record}*/
+void RequestTransferExit::requestTRansferExitRequest(canid_t can_id, const std::vector<uint8_t>& request_transfer_exit_data)
+{
+    std::vector<uint8_t> response;
+    /* Check the frame data */
+    if (request_transfer_exit_data.size() < 3)
+    {
+        /* Incorrect message length or invalid format - prepare a negative response */
+        uint8_t nrc = 0x13; 
+        /* Send the negative response frame */        
+        generate_frames.negativeResponse(can_id, RTES_SERVICE_ID, nrc);
+        return;
+    }
+    else    
+    {
+        /* Retrieve transfer_status based on the OTA_UPDATE_STATUS_DID if it exists */
+        if ( MCU::mcu.mcu_data.find(0x01E0) != MCU::mcu.mcu_data.end())
+        {
+            uint8_t transfer_status = MCU::mcu.mcu_data.at(0x01E0)[0];
+            /* Check if the transfer data has been completed */
+            if (transfer_status == 0x31)        
+                {
+                /* prepare positive response */
+                response.push_back(0x02); /* PCI */
+                response.push_back(0x77); /* Service ID */
+                response.push_back(request_transfer_exit_data[2]);
+                /* Send the postive response frame */ 
+                generate_frames.sendFrame(can_id, response);	
+                }
+                else
+                {                                
+                /* Request sequence error - prepare a negative response */
+                uint8_t nrc = 0x24;
+                /* Send the negative response frame */ 
+                generate_frames.negativeResponse(can_id, RTES_SERVICE_ID, nrc);
+                return;
+                }       
+        }
+        else
+        {
+           /*  OTA_UPDATE_STATUS_DID not found */
+            LOG_ERROR(RTESLogger.GET_LOGGER(),"OTA_UPDATE_STATUS_DID 01E0 not found "); 
+        }
+    }
 }
