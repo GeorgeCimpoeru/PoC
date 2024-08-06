f5a02e20a9f072c5f6e12e234a6ee687ec5a33db
Elisei-26 <pralaelisei76@gmail.com>
2024-08-06 12:30:09 +0300

    Add CAN control interface
    Implemented the design and features of CAN control interface

--- src/front_end/carsdata/app/components/UDScomponents/DivLeft.tsx
+++ src/front_end/carsdata/app/components/UDScomponents/DivLeft.tsx
@@ -1,42 +1,21 @@
 'use client'
-import Image from 'next/image';
 import React, { useState } from 'react';
 
 const DivLeft = (props: any) => {
     let [activeIcon, setActiveIcon] = useState(props.selectedModule);
 
-    const handleIconClick = (iconIndex: number) => {
-        activeIcon = iconIndex;
-    };
-
-    const handleSendBack = async (iconIndex: string) => {
-        props.changeSelectedModule(iconIndex)
-        // Modify the value if needed
-        const modifiedValue = iconIndex;
+    const handleSendBack = (iconIndex: string) => {
+        props.changeSelectedModule(iconIndex);
         setActiveIcon(iconIndex);
-
-        // Send the modified value back to the server
-        const response = await fetch('/UDS', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(modifiedValue),
-        });
-
-        // if (response.ok) {
-        //     console.log('Value sent back to the server successfully');
-        // } else {
-        //     console.error('Failed to send value back to the server');
-        // }
     };
 
     return (
-        <div className="w-[10%] bg-blue-950 p-6 shadow-xl flex flex-col justify-center space-y-4">
+        <div className="w-[10%] h-screen bg-blue-950 p-6 shadow-xl flex flex-col justify-center space-y-4">
             <div onClick={() => handleSendBack("1")} className={activeIcon === "1" ? "bg-white/50 p-1 rounded-lg" : "text-cyan-50"}>MCU</div>
             <div onClick={() => handleSendBack("2")} className={activeIcon === "2" ? "bg-white/50 p-1 rounded-lg" : "text-cyan-50"}>Battery</div>
             <div onClick={() => handleSendBack("3")} className={activeIcon === "3" ? "bg-white/50 p-1 rounded-lg" : "text-cyan-50"}>Engine</div>
             <div onClick={() => handleSendBack("4")} className={activeIcon === "4" ? "bg-white/50 p-1 rounded-lg" : "text-cyan-50"}>SOTA/SIT</div>
+            <div onClick={() => handleSendBack("5")} className={activeIcon === "5" ? "bg-white/50 p-1 rounded-lg" : "text-cyan-50"}>Send requests</div>
         </div>
     );
 }
