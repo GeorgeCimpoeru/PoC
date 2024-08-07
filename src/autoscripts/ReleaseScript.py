80eaccf338bb5e115205675c3d95519275ec83a2
Daniel Iancu <iancudaniel99@yahoo.com>
2024-08-07 13:22:35 +0300

    fix path finding problems in release script

--- src/autoscripts/ReleaseScript.py
+++ src/autoscripts/ReleaseScript.py
@@ -8,63 +8,62 @@ import shutil
 # example: python3 ReleaseScript.py 1 all
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.getcwd(), "..", ".."))
-path_tool = ""
-
+PATH_SOFTWARE_RELEASES = PROJECT_PATH + '/software_releases'
 
 def find_path():
     # Search in most impotrtantdir
-    possible_paths = ["~/PoC/tools", "~/Desktop/PoC/tools"]
+    possible_paths = ["~/PoC/software_releases", "~/Desktop/PoC/software_releases"]
     for path in possible_paths:
         if os.system("cd {0} 2>/dev/null".format(path)) == 0:
             global path_tool
-            path_tool = path
+            PATH_SOFTWARE_RELEASES = path
             return
     # Search in the whole user dir
     result = subprocess.run("find ~ -type d -name PoC 2>/dev/null",
                             shell=True, stdout=subprocess.PIPE, text=True)
     if str(result.stdout) != "":
-        path_tool = str(result.stdout)[:-1]+"/tools"
+        PATH_SOFTWARE_RELEASES = str(result.stdout)[:-1]+"/software_releases"
         return
     # Unable to find
-    print("Unable to find the PoC/tool dir, please pass the path as second argument")
+    print("Unable to find the PoC/software_releases dir, please pass the path as second argument")
     exit(-1)
 
 
 def checking():
     r = subprocess.run('pwd', shell=True, stdout=subprocess.PIPE, text=True)
     current_dir = str(r.stdout)[-10:-1]
-    if current_dir != "PoC/tools":
+    if current_dir != "PoC/software_releases":
         find_path()
 
 
 def create_exec(version: str, sw_to_build: str):
     dir_name = "release_" + version + '_' + sw_to_build
-    directory_path = os.path.expanduser(path_tool + "/"+dir_name)
+    directory_path = os.path.expanduser(PATH_SOFTWARE_RELEASES + "/"+dir_name)
 
-    if subprocess.run(["ls {0}/{1} 2>/dev/null".format(path_tool, dir_name)], shell=True).returncode == 0:
-        print("This release v{0} alrready exists".format(version))
+    if subprocess.run(["ls {0}/{1} 2>/dev/null".format(PATH_SOFTWARE_RELEASES, dir_name)], shell=True).returncode == 0:
+        print("This release_{0}_{1} already exists".format(version, sw_to_build))
         exit(-1)
     try:
         # Create the directory
         subprocess.run(
-            ["mkdir", "-p", "{0}/{1}".format(path_tool, dir_name)], check=True)
+            ["mkdir", "-p", "{0}/{1}".format(PATH_SOFTWARE_RELEASES, dir_name)], check=True)
         if sw_to_build == "mcu" or sw_to_build == "all":
             # Build the MCU executable
             subprocess.run(
-                ["make", "-C", "{0}/../src/mcu/".format(path_tool)], check=True)
-            mcu_executable_path = "{0}/../src/mcu/main".format(path_tool)
+                ["make", "-C", "{0}/../src/mcu/".format(PATH_SOFTWARE_RELEASES)], check=True)
+            mcu_executable_path = "{0}/../src/mcu/main".format(PATH_SOFTWARE_RELEASES)
             mcu_archive_name = "{0}/{1}/MCU_SW_VERSION_{2}".format(
-                path_tool, dir_name, version)
+                PATH_SOFTWARE_RELEASES, dir_name, version)
             shutil.make_archive(mcu_archive_name, 'zip', root_dir=os.path.dirname(
                 mcu_executable_path), base_dir=os.path.basename(mcu_executable_path))
         if sw_to_build == "ecu" or sw_to_build == "all":
             # Build the Battery Module executable
             subprocess.run(
-                ["make", "-C", "{0}/../src/ecu_simulation/BatteryModule".format(path_tool)], check=True)
+                ["make", "-C", "{0}/../src/ecu_simulation/BatteryModule".format(PATH_SOFTWARE_RELEASES)], check=True)
             battery_executable_path = "{0}/../src/ecu_simulation/BatteryModule/main".format(
-                path_tool)
+                PATH_SOFTWARE_RELEASES)
             battery_archive_name = "{0}/{1}/ECU_BATTERY_SW_VERSION_{2}".format(
-                path_tool, dir_name, version)
+                PATH_SOFTWARE_RELEASES, dir_name, version)
             shutil.make_archive(battery_archive_name, 'zip', root_dir=os.path.dirname(
                 battery_executable_path), base_dir=os.path.basename(battery_executable_path))
 
@@ -84,20 +83,17 @@ def uploadRelease(directory_path: str):
         if "MCU" in file:
             gDrive.uploadFile(file, os.path.join(
                 directory_path, file), DRIVE_MCU_SW_VERSIONS_FILE)
+            print('${file} UPLOADED TO GOOGLE DRIVE')
         elif "BATTERY" in file:
             gDrive.uploadFile(file, os.path.join(directory_path, file),
                               DRIVE_ECU_BATTERY_SW_VERSIONS_FILE)
-
-    print("\n--------- SW Versions Uploaded ----------")
-
+            print('${file} UPLOADED TO GOOGLE DRIVE')
 
 def main():
-    global path_tool
     version = "1"
     sw_to_build = "all"
     if len(sys.argv) > 1:
         version = sys.argv[1]
-        checking()
     elif len(sys.argv) <= 1:
         print("Please provide as argument the version of the release")
         # exit(-1)
