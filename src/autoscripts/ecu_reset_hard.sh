cc04663c6d2d45c14e13ace912e9fc3e14a41fa1
TheoRandstad <theodor.stoica@randstaddigital.com>
2024-08-16 02:50:30 +0300

    removed com

--- src/autoscripts/ecu_reset_hard.sh
+++ src/autoscripts/ecu_reset_hard.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Process name
 PROCESS_NAME="main_mcu"
 
 # Temporary file to store `top` output
 TMP_FILE=$(mktemp)
 
-# Capture `top` output in batch mode
 top -b -n 1 > "$TMP_FILE"
 
-# Extract PIDs of the specified process name
 PIDS=$(awk -v process="$PROCESS_NAME" '$12 ~ process {print $1}' "$TMP_FILE")
 
-# Check if any PIDs were found
 if [ -n "$PIDS" ]; then
     echo "Killing processes with PIDs: $PIDS"
-    # Kill each PID
     for PID in $PIDS; do
         kill -9 "$PID"
     done
@@ -28,19 +23,17 @@ rm -f "$TMP_FILE"
 ./main_mcu
 
 
-# Process name
+
+
 PROCESS_NAME="main_battery"
 
 # Temporary file to store `top` output
 TMP_FILE=$(mktemp)
 
-# Capture `top` output in batch mode
 top -b -n 1 > "$TMP_FILE"
 
-# Extract PIDs of the specified process name
 PIDS=$(awk -v process="$PROCESS_NAME" '$12 ~ process {print $1}' "$TMP_FILE")
 
-# Check if any PIDs were found
 if [ -n "$PIDS" ]; then
     echo "Killing processes with PIDs: $PIDS"
     # Kill each PID
