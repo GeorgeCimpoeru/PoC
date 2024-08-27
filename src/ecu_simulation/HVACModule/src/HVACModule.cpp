13d421186a6a94de14340f5989d8186ad52f454a
IDaniel99 <iancudaniel99@yahoo.com>
2024-08-27 14:33:41 +0300

    add init function for hvac

--- src/ecu_simulation/HVACModule/src/HVACModule.cpp
+++ src/ecu_simulation/HVACModule/src/HVACModule.cpp
@@ -4,7 +4,30 @@ Logger *hvacModuleLogger = nullptr;
 
 HVACModule::HVACModule()
 {
+    initHVAC();
+}
+
+void HVACModule::initHVAC()
+{
     _ecu = new ECU(HVAC_ECU_ID, _logger);
+
+    /* Insert the default DID values in the file */
+    std::ofstream outfile("hvac_data.txt");
+    if (!outfile.is_open())
+    {
+        throw std::runtime_error("Failed to open file: hvac_data.txt");
+    }
+
+    for (const auto& [data_identifier, data] : hvac_data)
+    {
+        outfile << std::hex << std::setw(4) << std::setfill('0') << data_identifier << " ";
+        for (uint8_t byte : data) 
+        {
+            outfile << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte) << " ";
+        }
+        outfile << "\n";
+    }
+    outfile.close();
 }
 
 int HVACModule::getSocket()
