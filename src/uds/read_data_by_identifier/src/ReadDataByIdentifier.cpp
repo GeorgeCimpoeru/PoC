b332de9ae185ea6e54b2d490793275ac672ea6a2
TheoRandstad <theodor.stoica@randstaddigital.com>
2024-08-31 16:59:28 +0300

    rdbi wrong nrc because of a duplicated line. Found during testing

--- src/uds/read_data_by_identifier/src/ReadDataByIdentifier.cpp
+++ src/uds/read_data_by_identifier/src/ReadDataByIdentifier.cpp
@@ -70,8 +70,6 @@ std::vector<uint8_t> ReadDataByIdentifier::readDataByIdentifier(canid_t frame_id
         response.push_back(0x7F); /* Negative response */
         response.push_back(RDBI_SERVICE_ID); /* Service ID */
         response.push_back(NegativeResponse::IMLOIF); /* Incorrect message length or invalid format */
-        response.push_back(NegativeResponse::IMLOIF); /* Incorrect message length or invalid format */
-
         if (use_send_frame)
         {
             /* Send the negative response frame */ 
