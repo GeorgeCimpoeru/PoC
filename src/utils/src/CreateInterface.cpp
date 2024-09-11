35867c5cba1ca44d47d5964a205e5b8c410b4720
IDaniel99 <iancudaniel99@yahoo.com>
2024-09-11 13:38:18 +0300

    change cmd name

--- src/utils/src/CreateInterface.cpp
+++ src/utils/src/CreateInterface.cpp
@@ -55,8 +55,8 @@ bool CreateInterface::createInterface()
     /* flag to track the succes or failure of each command */
     bool command_check = true;
 
-    std::string command = "ip link show vcan" + std::to_string(first_four_bits) + " > /dev/null 2>&1";
-    if(system(command.c_str()) != 0)
+    std::string command_check_vcan_exists = "ip link show vcan" + std::to_string(first_four_bits) + " > /dev/null 2>&1";
+    if(system(command_check_vcan_exists.c_str()) != 0)
     {
         /* Create interface command for the first interface (for ECU communication) */
         std::string cmd_ecu = "sudo ip link add vcan" + std::to_string(first_four_bits) + " type vcan";   
@@ -67,8 +67,8 @@ bool CreateInterface::createInterface()
             command_check  = false;
         }
     }
-    command = "ip link show vcan" + std::to_string(first_four_bits) + " >/dev/null 2>&1";
-    if(system(command.c_str()) != 0)
+    command_check_vcan_exists = "ip link show vcan" + std::to_string(first_four_bits) + " >/dev/null 2>&1";
+    if(system(command_check_vcan_exists.c_str()) != 0)
     {
         /* Create interface command for the second interface (for API communication) */
         std::string cmd_api = "sudo ip link add vcan" + std::to_string(last_four_bits) + " type vcan";
