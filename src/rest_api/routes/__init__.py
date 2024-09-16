2cae36937f77ecda6b9db5774bb11dd7dc05bd5d
DanielConstantinescu <daniel.constantinescu94@gmail.com>
2024-09-16 13:09:38 +0300

    Cleanup

--- src/rest_api/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-api_bp = Blueprint('api', __name__)
-main_bp = Blueprint('main', __name__)
--- src/rest_api/routes/main.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from . import main_bp
-
-
-@main_bp.route('/')
-def index():
-    return render_template('index.html')
