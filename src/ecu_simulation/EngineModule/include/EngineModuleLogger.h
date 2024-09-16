794ef5b972793a1ca517ef4c378dc5bd0e69b444
DanielConstantinescu <daniel.constantinescu94@gmail.com>
2024-09-16 12:14:55 +0300

    DOamne ajuta

--- src/ecu_simulation/EngineModule/include/EngineModuleLogger.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENGINEMODULELOGGER_H
-#define ENGINEMODULELOGGER
-
-#include "../../../utils/include/Logger.h"
-
-/* Global logger for the Engine Module */
-extern Logger* engineModuleLogger;
-
-#endif
