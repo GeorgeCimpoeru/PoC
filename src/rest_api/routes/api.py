b197037c936887ef57756ef764b3da7a4362576d
Adrian Savu <adrian.savu@randstaddigital.com>
2024-09-18 13:12:26 +0300

    flake8 fix

--- src/rest_api/routes/api.py
+++ src/rest_api/routes/api.py
@@ -10,7 +10,7 @@ from actions.read_info_action import *  # noqa: E402
 from utils.logger import log_memory  # noqa: E402
 from actions.manual_send_frame import manual_send_frame  # noqa: E402
 from actions.write_info_action import WriteInfo  # noqa: E402
-# from src.ota.google_drive_api.GoogleDriveApi import gDrive  # noqa: E402
+from src.ota.google_drive_api.GoogleDriveApi import gDrive  # noqa: E402
 from actions.secure_auth import Auth  # noqa: E402
 from actions.dtc_info import DiagnosticTroubleCode  # noqa: E402
 from actions.diag_session import SessionManager  # noqa: E402
@@ -94,10 +94,10 @@ def get_logs():
 
 
 # Google Drive API Endpoints
-# @api_bp.route('/drive_update_data', methods=['GET'])
-# def update_drive_data():
-#     drive_data_json = gDrive.getDriveData()
-#     return jsonify(drive_data_json)
+@api_bp.route('/drive_update_data', methods=['GET'])
+def update_drive_data():
+    drive_data_json = gDrive.getDriveData()
+    return jsonify(drive_data_json)
 
 
 @api_bp.route('/authenticate', methods=['GET'])
@@ -215,4 +215,3 @@ def write_timing():
     result = writer._write_timing_info(id, timing_values)
 
     return jsonify(result)
-
