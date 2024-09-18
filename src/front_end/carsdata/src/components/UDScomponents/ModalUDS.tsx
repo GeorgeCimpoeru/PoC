6435355e125be23a604fc1aa5ce693676ac5346e
Elisei-26 <pralaelisei76@gmail.com>
2024-09-18 10:50:33 +0300

    Fix issues made by merges between branches

--- src/front_end/carsdata/src/components/UDScomponents/ModalUDS.tsx
+++ src/front_end/carsdata/src/components/UDScomponents/ModalUDS.tsx
@@ -8,7 +8,6 @@ const ModalUDS = (props: any) => {
     }
 
     const handleInputClick = () => {
-    const handleInputClick = () => {
         setInput('');
     }
 
--- src/front_end/carsdata/src/components/UDScomponents/SendRequests.tsx
+++ src/front_end/carsdata/src/components/UDScomponents/SendRequests.tsx
@@ -200,36 +200,6 @@ const SendRequests = () => {
         removeLoadingCicle();
     }
 
-
-    const testerPresent = async () => {
-        const displayPopup = () => {
-            const popup = document.createElement('div');
-            popup.innerText = "Tester present is still active";
-            popup.style.position = 'fixed';
-            popup.style.top = '50%';
-            popup.style.left = '50%';
-            popup.style.transform = 'translate(-50%, -50%)';
-            popup.style.padding = '20px';
-            popup.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-            popup.style.color = 'white';
-            popup.style.borderRadius = '10px';
-            popup.style.zIndex = '1000';
-            popup.style.textAlign = 'center';
-
-            document.body.appendChild(popup);
-
-            setTimeout(() => {
-                document.body.removeChild(popup);
-            }, 4000);
-        };
-
-        displayPopup();
-        // setInterval(() => {
-        //     displayPopup();
-        // }, 10000);
-    };
-
-
     const requestIds = async (initialRequest: boolean) => {
         displayLoadingCircle();
         console.log("Requesting ids...");
