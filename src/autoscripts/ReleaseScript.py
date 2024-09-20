c9954afe30a6f4b25f7d29bc77bfb6f36facc732
IDaniel99 <iancudaniel99@yahoo.com>
2024-09-20 18:19:05 +0300

    add new modules to releasescript and add uncompressed size field

--- src/autoscripts/ReleaseScript.py
+++ src/autoscripts/ReleaseScript.py
@@ -28,6 +28,11 @@ RESET = '\033[0m'
 PROJECT_PATH = os.path.abspath(os.path.join(os.getcwd(), "..", ".."))
 PATH_SOFTWARE_RELEASES = PROJECT_PATH + '/software_releases'
 
+mcu_size_uncompressed = 0
+battery_size_uncompressed = 0
+engine_size_uncompressed = 0
+doors_size_uncompressed = 0
+hvac_size_uncompressed = 0
 
 def create_exec(version: str, sw_to_build: str, extra_args: str):
     dir_name = "release_" + sw_to_build + '_' + version
@@ -55,8 +60,6 @@ def create_exec(version: str, sw_to_build: str, extra_args: str):
             ["mkdir", "-p", "{0}/{1}".format(PATH_SOFTWARE_RELEASES, dir_name)], check=True)
         if sw_to_build == "mcu" or sw_to_build == "all":
             if "clean" in extra_args:
-                print(extra_args)
-
                 subprocess.run(
                     ["sudo", "make", "clean", "-C", "{0}/../src/mcu/".format(PATH_SOFTWARE_RELEASES)], check=True)
             # Build the MCU executable
@@ -66,9 +69,9 @@ def create_exec(version: str, sw_to_build: str, extra_args: str):
                 PATH_SOFTWARE_RELEASES)
             mcu_archive_name = "{0}/{1}/MCU_SW_VERSION_{2}".format(
                 PATH_SOFTWARE_RELEASES, dir_name, version)
-            shutil.make_archive(mcu_archive_name, 'zip', root_dir=os.path.dirname(
-                mcu_executable_path), base_dir=os.path.basename(mcu_executable_path))
-        if sw_to_build == "ecu" or sw_to_build == "all":
+            global mcu_size_uncompressed 
+            mcu_size_uncompressed = createArchive(mcu_executable_path, mcu_archive_name)
+        if sw_to_build == "battery" or sw_to_build == "all":
             if "clean" in extra_args:
                 subprocess.run(
                     ["sudo", "make", "clean", "-C", "{0}/../src/ecu_simulation/BatteryModule".format(PATH_SOFTWARE_RELEASES)], check=True)
@@ -79,8 +82,47 @@ def create_exec(version: str, sw_to_build: str, extra_args: str):
                 PATH_SOFTWARE_RELEASES)
             battery_archive_name = "{0}/{1}/ECU_BATTERY_SW_VERSION_{2}".format(
                 PATH_SOFTWARE_RELEASES, dir_name, version)
-            shutil.make_archive(battery_archive_name, 'zip', root_dir=os.path.dirname(
-                battery_executable_path), base_dir=os.path.basename(battery_executable_path))
+            global battery_size_uncompressed
+            battery_size_uncompressed = createArchive(battery_executable_path, battery_archive_name)
+        if sw_to_build == "engine" or sw_to_build == "all":
+            if "clean" in extra_args:
+                subprocess.run(
+                    ["sudo", "make", "clean", "-C", "{0}/../src/ecu_simulation/BatteryModule".format(PATH_SOFTWARE_RELEASES)], check=True)
+            # Build the engine Module executable
+            subprocess.run(
+                ["sudo", "make", "-C", "{0}/../src/ecu_simulation/EngineModule".format(PATH_SOFTWARE_RELEASES)], check=True)
+            engine_executable_path = "{0}/../src/ecu_simulation/EngineModule/main_engine".format(
+                PATH_SOFTWARE_RELEASES)
+            engine_archive_name = "{0}/{1}/ECU_ENGINE_SW_VERSION_{2}".format(
+                PATH_SOFTWARE_RELEASES, dir_name, version)
+            global engine_size_uncompressed
+            engine_size_uncompressed = createArchive(engine_executable_path, engine_archive_name)
+        if sw_to_build == "doors" or sw_to_build == "all":
+            if "clean" in extra_args:
+                subprocess.run(
+                    ["sudo", "make", "clean", "-C", "{0}/../src/ecu_simulation/DoorsModule".format(PATH_SOFTWARE_RELEASES)], check=True)
+            # Build the doors Module executable
+            subprocess.run(
+                ["sudo", "make", "-C", "{0}/../src/ecu_simulation/DoorsModule".format(PATH_SOFTWARE_RELEASES)], check=True)
+            doors_executable_path = "{0}/../src/ecu_simulation/DoorsModule/main_doors".format(
+                PATH_SOFTWARE_RELEASES)
+            doors_archive_name = "{0}/{1}/ECU_DOORS_SW_VERSION_{2}".format(
+                PATH_SOFTWARE_RELEASES, dir_name, version)
+            global doors_size_uncompressed
+            doors_size_uncompressed = createArchive(doors_executable_path, doors_archive_name)
+        if sw_to_build == "hvac" or sw_to_build == "all":
+            if "clean" in extra_args:
+                subprocess.run(
+                    ["sudo", "make", "clean", "-C", "{0}/../src/ecu_simulation/HVACModule".format(PATH_SOFTWARE_RELEASES)], check=True)
+            # Build the hvac Module executable
+            subprocess.run(
+                ["sudo", "make", "-C", "{0}/../src/ecu_simulation/HVACModule".format(PATH_SOFTWARE_RELEASES)], check=True)
+            hvac_executable_path = "{0}/../src/ecu_simulation/HVACModule/main_hvac".format(
+                PATH_SOFTWARE_RELEASES)
+            hvac_archive_name = "{0}/{1}/ECU_HVAC_SW_VERSION_{2}".format(
+                PATH_SOFTWARE_RELEASES, dir_name, version)
+            global hvac_size_uncompressed
+            hvac_size_uncompressed = createArchive(hvac_executable_path, hvac_archive_name)
         user_input = input(f"{GREEN}Upload to Google Drive? y/n: {RESET}")
         if user_input.lower() == 'y':
             uploadRelease(directory_path)
@@ -94,19 +136,36 @@ def create_exec(version: str, sw_to_build: str, extra_args: str):
         print(error)
     return directory_path
 
-
+def createArchive(executable_path, archive_name):
+    size_uncompressed = os.path.getsize(executable_path)
+    shutil.make_archive(archive_name, 'zip', root_dir=os.path.dirname(
+                executable_path), base_dir=os.path.basename(executable_path))
+    return size_uncompressed
+    
 def uploadRelease(directory_path: str):
     google_drive_api_path = PROJECT_PATH + '/src/ota/google_drive_api'
     sys.path.append(google_drive_api_path)
-    from GoogleDriveApi import gDrive, DRIVE_MCU_SW_VERSIONS_FILE, DRIVE_ECU_BATTERY_SW_VERSIONS_FILE
+    from GoogleDriveApi import gDrive, DRIVE_MCU_SW_VERSIONS_FILE, DRIVE_ECU_BATTERY_SW_VERSIONS_FILE, DRIVE_ECU_ENGINE_SW_VERSIONS_FILE, DRIVE_ECU_DOORS_SW_VERSIONS_FILE, DRIVE_ECU_HVAC_SW_VERSIONS_FILE 
 
     for file in os.listdir(directory_path):
+
         if "MCU" in file:
+            print(os.path.join(
+                directory_path, file))
             gDrive.uploadFile(file, os.path.join(
-                directory_path, file), DRIVE_MCU_SW_VERSIONS_FILE)
+                directory_path, file), mcu_size_uncompressed, DRIVE_MCU_SW_VERSIONS_FILE)
         elif "BATTERY" in file:
-            gDrive.uploadFile(file, os.path.join(directory_path, file),
-                              DRIVE_ECU_BATTERY_SW_VERSIONS_FILE)
+            gDrive.uploadFile(file, os.path.join(
+                directory_path, file), battery_size_uncompressed, DRIVE_ECU_BATTERY_SW_VERSIONS_FILE)
+        elif "ENGINE" in file:
+            gDrive.uploadFile(file, os.path.join(
+                directory_path, file), engine_size_uncompressed, DRIVE_ECU_ENGINE_SW_VERSIONS_FILE)
+        elif "DOORS" in file:
+            gDrive.uploadFile(file, os.path.join(
+                directory_path, file), doors_size_uncompressed, DRIVE_ECU_DOORS_SW_VERSIONS_FILE)
+        elif "HVAC" in file:
+            gDrive.uploadFile(file, os.path.join(
+                directory_path, file), hvac_size_uncompressed, DRIVE_ECU_HVAC_SW_VERSIONS_FILE)
 
 
 def validateSoftwareVersion(software_version):
