47d6ea00c229d355fe3b027619fc3cb1953d45de
Elisei-26 <pralaelisei76@gmail.com>
2024-09-25 16:07:45 +0300

    Uncomment UDSactivity code

--- src/android_app/CarsData/app/build.gradle.kts
+++ src/android_app/CarsData/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
