175feb75d5f6b2b9202abb675c3a4b260bc60890
Elisei-26 <81551154+Elisei-26@users.noreply.github.com>
2024-09-30 15:04:32 +0300

    Feature/android -> feature/front (#425)
    * new Android application for PoC
    
    * OTA activity
    
    * Navbar and logic
    
    * Draft design for HomePage
    
    * Create fragments
    
    * Ativity UDS design created
    
    * Uncomment UDSactivity code
    
    * updated with fragments
    
    * Some fixes
    
    ---------
    
    Co-authored-by: Miruna-Maria <miruna.stoisor@randstaddigital.com>
    Co-authored-by: RubenNatanael <natanael_ruben@hotmail.com>
    Co-authored-by: RubenNatanael <135959195+RubenNatanael@users.noreply.github.com>

---
+++ src/android_app/CarsData/app/src/main/java/com/poc/p_couds/Fragment_RequestSend.java
@@ -0,0 +1,25 @@
+package com.poc.p_couds;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment_RequestSend#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment_RequestSend extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_request_send, container, false);
+    }
+}
