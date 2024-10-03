6878b8c2106db6382b7654d15ade487aabd7a956
CatalinGP <catalin.popescu@randstaddigital.com>
2024-10-03 17:55:31 +0300

    multiprocessing to start can2udp2can.py script with python3

--- src/rest_api/app.py
+++ src/rest_api/app.py
@@ -4,7 +4,8 @@ from config import *
 from routes.main import main_bp
 from flask_cors import CORS
 from routes.api import api_bp
-
+import multiprocessing
+import subprocess
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,5 +18,21 @@ app.register_blueprint(main_bp)
 CORS(app)
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
+    # app.run(port=5001, debug=True)
+
+def run_flask_app():
     app.run(port=5001, debug=True)
+
+def run_can2udp():
+    subprocess.run(['python3', 'can2udp2can.py'])
+
+if __name__ == '__main__':
+    flask_process = multiprocessing.Process(target=run_flask_app)
+    can_process = multiprocessing.Process(target=run_can2udp)
+
+    flask_process.start()
+    can_process.start()
+
+    flask_process.join()
+    can_process.join()
