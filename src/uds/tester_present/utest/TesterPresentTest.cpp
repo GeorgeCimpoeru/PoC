acf274168dcf50fd7fc9bf793dd697c58d158112
TheoRandstad <theodor.stoica@randstaddigital.com>
2024-10-04 10:41:56 +0300

    100% coverage tester present

--- src/uds/tester_present/utest/TesterPresentTest.cpp
+++ src/uds/tester_present/utest/TesterPresentTest.cpp
@@ -1,5 +1,6 @@
 /**
- * @file tester_present_test.cpp
+ * @file TesterPresentTest.cpp
+ * @author Theodor Stoica
  * @brief Unit test for TesterPresent service
  * @version 0.1
  * @date 2024-07-03
@@ -20,29 +21,6 @@ int socket_;
 int socket2_;
 /*ID frame for response */
 const int id = 0x10FA;
-
-class CaptureFrame
-{
-    public:
-        struct can_frame frame;
-        void capture()
-        {
-            read(socket_, &frame, sizeof(struct can_frame));
-        }
-};
-
-struct can_frame createFrame(std::vector<uint8_t> test_data)
-{
-    struct can_frame result_frame;
-    result_frame.can_id = id;
-    int i = 0;
-    for (auto d : test_data)
-    {
-        result_frame.data[i++] = d;
-    }
-    result_frame.can_dlc = test_data.size();
-    return result_frame;
-}
 
 int createSocket()
 {
@@ -85,6 +63,34 @@ int createSocket()
     return s;
 }
 
+struct can_frame createFrame(std::vector<uint8_t> test_data)
+{
+    struct can_frame result_frame;
+    result_frame.can_id = id;
+    int i = 0;
+    for (auto d : test_data)
+    {
+        result_frame.data[i++] = d;
+    }
+    result_frame.can_dlc = test_data.size();
+    return result_frame;
+}
+
+bool containsLine(const std::string& output, const std::string& line)
+{
+    return output.find(line) != std::string::npos;
+}
+
+class CaptureFrame
+{
+    public:
+        struct can_frame frame;
+        void capture()
+        {
+            read(socket_, &frame, sizeof(struct can_frame));
+        }
+};
+
 void testFrames(struct can_frame expected_frame, CaptureFrame &c1 )
 {
     EXPECT_EQ(expected_frame.can_id && 0xFFFF, c1.frame.can_id && 0xFFFF);
@@ -95,30 +101,91 @@ void testFrames(struct can_frame expected_frame, CaptureFrame &c1 )
     }
 }
 
-bool containsLine(const std::string& output, const std::string& line)
-{
-    return output.find(line) != std::string::npos;
-}
-
 struct TesterPresentTest : testing::Test
 {
     TesterPresent* tp;
     CaptureFrame* c1;
     Logger* logger;
+    DiagnosticSessionControl* session;
     TesterPresentTest()
     {
         logger = new Logger();
-        DiagnosticSessionControl session(*logger, socket2_);
-        tp = new TesterPresent(socket2_, *logger,session);
+        session = new DiagnosticSessionControl(*logger, socket2_);
+        tp = new TesterPresent(socket2_, *logger,*session);
         c1 = new CaptureFrame();
+        /* Set session to default for each test */
+        session->sessionControl(0xFA10,0x01);
+        /* Read the frame to be ready to read the next frame */
+        c1->capture();
     }
     ~TesterPresentTest()
     {
         delete tp;
         delete c1;
+        delete session;
         delete logger;
     }
 };
+
+TEST_F(TesterPresentTest, SetEndTimeDefaultSession)
+{
+    tp->setEndTimeProgrammingSession();
+    auto expected_programming_end_time = std::chrono::steady_clock::now();
+    auto end_time = tp->getEndTimeProgrammingSession();
+    EXPECT_NEAR(std::chrono::duration_cast<std::chrono::hours>
+        (
+            end_time - expected_programming_end_time
+        ).count(),24 * 365, 1);
+}
+
+TEST_F(TesterPresentTest, SetEndTimeProgrammingSession)
+{
+    tp->setEndTimeProgrammingSession(true);
+    auto expected_programming_end_time = std::chrono::steady_clock::now();
+    auto end_time = tp->getEndTimeProgrammingSession();
+    EXPECT_NEAR(std::chrono::duration_cast<std::chrono::seconds>
+        (
+            end_time - expected_programming_end_time
+        ).count(),TesterPresent::S3_TIMER, 1);
+}
+
+TEST_F(TesterPresentTest, IncorrectMesssageLength)
+{
+    struct can_frame result_frame = createFrame({0x03, 0x7F, 
+                    TesterPresent::TESTER_PRESENT_SID, NegativeResponse::IMLOIF});
+
+    tp->handleTesterPresent(0xFA10, {0x04, 0x2E, 0x00});
+    c1->capture();
+    testFrames(result_frame, *c1);
+}
+
+TEST_F(TesterPresentTest, SubFunctionNotSupported)
+{
+    struct can_frame result_frame = createFrame({0x03, 0x7F, 
+                    TesterPresent::TESTER_PRESENT_SID, NegativeResponse::SFNS});
+
+    tp->handleTesterPresent(0xFA10, {0x02, 0x3E, 0x01});
+    c1->capture();
+    testFrames(result_frame, *c1);
+}
+
+TEST_F(TesterPresentTest, ProgrammingToProgrammingSession)
+{
+    struct can_frame result_frame = createFrame({0x02, 0x7E, 0x00});
+    session->sessionControl(0xFA10, 0x02);
+    c1->capture();
+    tp->handleTesterPresent(0xFA10, {0x02, 0x3E, 0x00});
+    c1->capture();
+    testFrames(result_frame, *c1);
+}
+
+TEST_F(TesterPresentTest, DefaultToProgrammingSession)
+{
+    struct can_frame result_frame = createFrame({0x02, 0x7E, 0x00});
+    tp->handleTesterPresent(0xFA10, {0x02, 0x3E, 0x00});
+    c1->capture();
+    testFrames(result_frame, *c1);
+}
 
 int main(int argc, char* argv[])
 {
@@ -128,7 +195,7 @@ int main(int argc, char* argv[])
     int result = RUN_ALL_TESTS();
     if (socket_ > 0)
     {
-        close(socket_);
+        close(socket_); 
     }
     if (socket2_ > 0)
     {
