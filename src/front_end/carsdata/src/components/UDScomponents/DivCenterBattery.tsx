8d354d034b2d700c564c757571dc7bd2156909ff
DanielConstantinescu <daniel.constantinescu94@gmail.com>
2024-10-14 12:20:46 +0300

    Feature/Frontend into DEV_Frontend (#464)
    * Fix for android
    
    * Update new Frontend branch (#433)
    
    * integrate read doors and hvac info endpoints (#435)
    
    * Split & update the code (#443)
    
    Code separation, removal of services from the standalone app, and modification of ECUs parameter values in relation to the new changes from the API
    
    * Database integration for the available versions part of OTA (#445)
    
    * integrate hvac write endpoint and change doors parameters (#451)
    
    * integrate hvac write endpoint and change doors parameters
    
    * fix api route
    
    * Feature/database improvements (#457)
    
    * Move available version in a single request
    
    * Improved database fetch for available versions
    
    * Feature/write engine (#458)
    
    * test
    
    * write data for engine
    
    completed the code in the standalone app and UDS page, updated modalUDS.tsx
    
    ---------
    
    Co-authored-by: andref00 <156180243+andref00@users.noreply.github.com>
    Co-authored-by: amalia94 <152972759+amalia94@users.noreply.github.com>

--- src/front_end/carsdata/src/components/UDScomponents/DivCenterBattery.tsx
+++ src/front_end/carsdata/src/components/UDScomponents/DivCenterBattery.tsx
@@ -112,7 +112,7 @@ const DivCenterBattery = (props: any) => {
         displayLoadingCircle();
         console.log("Reading battery info...");
         try {
-            await fetch(`http://127.0.0.1:5000/api/read_info_battery?identifier=`, {
+            await fetch(`http://127.0.0.1:5000/api/read_info_battery`, {
                 method: 'GET',
             }).then(response => response.json())
                 .then(data => {
@@ -137,33 +137,33 @@ const DivCenterBattery = (props: any) => {
 
     const writeInfoBattery = async (variable: string, newValue: string) => {
         console.log("Writing battery info...");
-        let identifier: string = "";
+        let item: string = "";
         let data2;
         if (variable === "battery_level") {
             data2 = {
                 energy_level: parseInt(newValue)
             };
-            identifier = "battery_level";
+            item = "battery_level";
         } else if (variable === "battery_state_of_charge") {
             data2 = {
                 state_of_charge: parseInt(newValue)
             };
-            identifier = "state_of_charge";
+            item = "state_of_charge";
         } else if (variable === "percentage") {
             data2 = {
                 percentage: parseInt(newValue)
             };
-            identifier = "percentage";
+            item = "percentage";
         } else if (variable === "voltage") {
             data2 = {
                 voltage: parseInt(newValue)
             };
-            identifier = "voltage";
+            item = "voltage";
         }
 
         console.log(data2);
         displayLoadingCircle();
-        await fetch(`http://127.0.0.1:5000/api/write_info_battery?identifier=${identifier}`, {
+        await fetch(`http://127.0.0.1:5000/api/write_info_battery?item=${item}`, {
             method: 'POST',
             // mode: 'no-cors',
             headers: {
@@ -185,23 +185,23 @@ const DivCenterBattery = (props: any) => {
 
     return (
         <div className="w-[65%] flex h-screen bg-indigo-950 math-paper" >
-            <div className="w-[35%] flex flex-col items-center">
+            <div className="w-[35%] flex flex-col item-center">
                 <h3 className="text-white text-3xl">Vehicle Model</h3>
-                <div className="w-full h-full flex flex-col items-center justify-center">
+                <div className="w-full h-full flex flex-col item-center justify-center">
 
-                    <div className="w-[30%] m-7 text-white grid justify-items-end">
+                    <div className="w-[30%] m-7 text-white grid justify-item-end">
                         <label htmlFor="my_modal_1"
-                            className="inline-flex items-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
+                            className="inline-flex item-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
                             {data?.battery_level}%
                         </label>
-                        <ModalUDS id="my_modal_1" cardTitle={'Battery level'} writeInfoBattery={writeInfoBattery} param="battery_level" />
+                        <ModalUDS id="my_modal_1" cardTitle={'Battery level'} writeInfo={writeInfoBattery} param="battery_level" />
                         <p>Battery level</p>
                     </div>
 
 
-                    <div className="w-[30%] m-7 grid justify-items-center">
+                    <div className="w-[30%] m-7 grid justify-item-center">
                         <div className="dropdown dropdown-end">
-                            <div tabIndex={0} role="button" className="btn m-1 text-white inline-flex items-center justify-center p-2 bg-red-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-red-700">
+                            <div tabIndex={0} role="button" className="btn m-1 text-white inline-flex item-center justify-center p-2 bg-red-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-red-700">
                                 {data?.battery_state_of_charge}
                             </div>
                             <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
@@ -217,18 +217,18 @@ const DivCenterBattery = (props: any) => {
                     </div>
 
 
-                    <div className="w-[30%] m-7 text-white grid justify-items-start">
+                    <div className="w-[30%] m-7 text-white grid justify-item-start">
                         <label htmlFor="my_modal_3"
-                            className="inline-flex items-center justify-center p-2 bg-green-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-green-700">
+                            className="inline-flex item-center justify-center p-2 bg-green-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-green-700">
                             {data?.charging_time}
                         </label>
                         {/* <ModalUDS id="my_modal_3" cardTitle={'Charging time'} /> */}
                         <p>Charging time</p>
                     </div>
 
-                    <div className="w-[30%] m-7 text-white grid justify-items-center">
+                    <div className="w-[30%] m-7 text-white grid justify-item-center">
                         <label htmlFor="my_modal_4"
-                            className="inline-flex items-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
+                            className="inline-flex item-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
                             {data?.device_consumption}
                         </label>
                         {/* <ModalUDS id="my_modal_4" cardTitle={'Device consumption'} /> */}
@@ -236,9 +236,9 @@ const DivCenterBattery = (props: any) => {
                     </div>
 
 
-                    <div className="w-[30%] m-7 text-white grid justify-items-end">
+                    <div className="w-[30%] m-7 text-white grid justify-item-end">
                         <label htmlFor="my_modal_5"
-                            className="inline-flex items-center justify-center p-2 bg-purple-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-purple-700">
+                            className="inline-flex item-center justify-center p-2 bg-purple-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-purple-700">
                             {data?.fully_charged}
                         </label>
                         {/* <ModalUDS id="my_modal_5" cardTitle={'Full charged'} /> */}
@@ -252,12 +252,12 @@ const DivCenterBattery = (props: any) => {
                 <Image src={props.image} alt="car sketch" width={400} height={204} className="invert" />
             </div>
 
-            <div className="w-[35%] flex flex-col items-center justify-center">
+            <div className="w-[35%] flex flex-col item-center justify-center">
 
 
                 <div className="w-[30%] m-7 text-white">
                     <label htmlFor="my_modal_6"
-                        className="inline-flex items-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
+                        className="inline-flex item-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
                         {data?.life_cycle}
                     </label>
                     {/* <ModalUDS id="my_modal_6" cardTitle={'Life cycle'} /> */}
@@ -265,9 +265,9 @@ const DivCenterBattery = (props: any) => {
                 </div>
 
 
-                <div className="w-[30%] m-7 text-white grid justify-items-center">
+                <div className="w-[30%] m-7 text-white grid justify-item-center">
                     <label htmlFor="my_modal_7"
-                        className="inline-flex items-center justify-center p-2 bg-red-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-red-700">
+                        className="inline-flex item-center justify-center p-2 bg-red-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-red-700">
                         {data?.life_cycle}
                     </label>
                     {/* <ModalUDS id="my_modal_7" cardTitle={'Life cycle'} /> */}
@@ -275,9 +275,9 @@ const DivCenterBattery = (props: any) => {
                 </div>
 
 
-                <div className="w-[30%] m-7 text-white grid justify-items-end">
+                <div className="w-[30%] m-7 text-white grid justify-item-end">
                     <label htmlFor="my_modal_8"
-                        className="inline-flex items-center justify-center p-2 bg-green-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-green-700">
+                        className="inline-flex item-center justify-center p-2 bg-green-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-green-700">
                         {data?.percentage}%
                     </label>
                     <ModalUDS id="my_modal_8" cardTitle={'Battery percentage'} writeInfoBattery={writeInfoBattery} param="percentage" />
@@ -285,9 +285,9 @@ const DivCenterBattery = (props: any) => {
                 </div>
 
 
-                <div className="w-[30%] m-7 text-white grid justify-items-center">
+                <div className="w-[30%] m-7 text-white grid justify-item-center">
                     <label htmlFor="my_modal_9"
-                        className="inline-flex items-center justify-center p-2 bg-purple-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-purple-700">
+                        className="inline-flex item-center justify-center p-2 bg-purple-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-purple-700">
                         {data?.serial_number}
                     </label>
                     {/* <ModalUDS id="my_modal_9" cardTitle={'Serial number'} /> */}
@@ -296,7 +296,7 @@ const DivCenterBattery = (props: any) => {
 
                 <div className="w-[30%] m-7 text-white">
                     <label htmlFor="my_modal_10"
-                        className="inline-flex items-center justify-center p-2 bg-purple-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-purple-700">
+                        className="inline-flex item-center justify-center p-2 bg-purple-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-purple-700">
                         {data?.voltage}V
                     </label>
                     <ModalUDS id="my_modal_10" cardTitle={'Voltage'} writeInfoBattery={writeInfoBattery} param="voltage" />
