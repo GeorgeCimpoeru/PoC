e70f8f213865e5db0baae8a836dd7286aa5e8c31
Miruna-Maria <167984320+Miruna-Maria@users.noreply.github.com>
2024-10-24 11:24:05 +0300

    Integrated endpoints in Send Request Fragment (#505)

--- src/android_app/CarsData/app/src/main/java/com/poc/p_couds/pojo/WriteTiming.java
+++ src/android_app/CarsData/app/src/main/java/com/poc/p_couds/pojo/WriteTiming.java
@@ -2,38 +2,33 @@ package com.poc.p_couds.pojo;
 
 import com.google.gson.annotations.SerializedName;
 
-public class ReadAccesTiming {
+public class WriteTiming {
     @SerializedName("message")
     private String message;
 
     @SerializedName("status")
     private String status;
 
-    @SerializedName("timing_values")
+    @SerializedName("written_values")
     private TimingValue timingValue;
 
     public static class TimingValue
     {
         // For subFunct = 3 (custom values)
-        @SerializedName("p2_max_time")
+        @SerializedName("New P2 Max Time")
         private String p2Max;
 
-        @SerializedName("p2_star_max_time")
+        @SerializedName("New P2 Star Max")
         private String p2Star;
 
-        @SerializedName("P2_MAX_TIME_DEFAULT")
-        private String p2MaxDefault;
-
-        @SerializedName("P2_STAR_MAX_TIME_DEFAULT")
-        private String p2StarDefault;
 
         public String getP2Max()
         {
-            return p2Max != null ? p2Max: p2MaxDefault;
+            return p2Max;
         }
         public String getP2Star()
         {
-            return p2Star != null ? p2Star: p2StarDefault;
+            return p2Star;
         }
     }
     public String getMessage()
