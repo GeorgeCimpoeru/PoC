b66bf97d1e61cff6f79c4d33f7b03a047e4e4716
IDaniel99 <iancudaniel99@yahoo.com>
2024-10-25 14:16:27 +0300

    fix pylint err

--- src/ota/google_drive_api/GoogleDriveApi.py
+++ src/ota/google_drive_api/GoogleDriveApi.py
@@ -76,7 +76,7 @@ class GDriveAPI:
         file_metadata = {
             'name': file_name,
             'parents': [parent_folder_id],  # ID of the folder where you want to upload
-            'appProperties' : {
+            'appProperties': {
                 'size_uncompressed': str(size_uncompressed)
             }
         }
@@ -105,7 +105,7 @@ class GDriveAPI:
                 if file_to_download['name'] in file_name:
                     print(f"{GREEN}{file_name} already downloaded.{RESET}")
                     return int(file_to_download['size_uncompressed'])
-            
+
             print(f"{GREEN}Downloading..{RESET}")
             request = self.__drive_service.files().get_media(
                 fileId=file_to_download['id'])
@@ -134,7 +134,7 @@ class GDriveAPI:
               ecu_map[ecu_id] + ' ' + sw_version + f"{RESET}")
 
         self.getDriveData()
-        
+
         file_to_download = [
             data for data in self.__drive_data_array if data['type'] == ecu_map[ecu_id] and data['sw_version'] == str(sw_version)]
 
